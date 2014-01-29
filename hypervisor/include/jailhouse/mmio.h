dcc81377415b8f6a4f4601bd4a63836dc3451f78
Ivan Kolchin <ivan.kolchin@siemens.com>
2014-01-29 16:42:02 +0400

    core: Add functions to read/write field values of 32/64-bit registers
    The following aims are obtained using these functions: making a register
    description to be uniform, easy reading/writing field values without using bit
    operations, making the code more readable, preventing casual changes of
    register content.
    
    Signed-off-by: Ivan Kolchin <ivan.kolchin@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/include/jailhouse/mmio.h
+++ hypervisor/include/jailhouse/mmio.h
@@ -10,6 +10,9 @@
  * the COPYING file in the top-level directory.
  */
 
+#ifndef _JAILHOUSE_MMIO_H
+#define _JAILHOUSE_MMIO_H
+
 #include <asm/percpu.h>
 
 struct mmio_access {
@@ -40,3 +43,53 @@ static inline void mmio_write64(void *address, u64 value)
 
 struct mmio_access mmio_parse(struct per_cpu *cpu_data, unsigned long pc,
 			      unsigned long page_table_addr, bool is_write);
+
+/**
+ * mmio_read32_field() - Read value of 32-bit register field
+ * @addr:	Register address.
+ * @mask:	Bit mask. Shifted value must be provided which describes both
+ * 		starting bit position (1st non-zero bit) and length of the field.
+ *
+ * Return: Field value of register.
+ */
+static inline u32 mmio_read32_field(void *addr, u32 mask)
+{
+	return (mmio_read32(addr) & mask) >> (__builtin_ffs(mask) - 1);
+}
+
+/**
+ * mmio_write32_field() - Write value of 32-bit register field
+ * @addr:	Register address.
+ * @mask:	Bit mask. See mmio_read32_field() for more details.
+ * @value:	Register field value (must be the same length as mask).
+ *
+ * Update field value of 32-bit register, leaving all other fields unmodified.
+
+ * Return: None.
+ */
+static inline void mmio_write32_field(void *addr, u32 mask, u32 value)
+{
+	mmio_write32(addr, (mmio_read32(addr) & ~mask) |
+			((value << (__builtin_ffs(mask) - 1)) & mask));
+}
+
+/**
+ * mmio_read64_field() - Read value of 64-bit register field.
+ * See mmio_read32_field() for more details.
+ */
+static inline u64 mmio_read64_field(void *addr, u64 mask)
+{
+	return (mmio_read64(addr) & mask) >> (__builtin_ffsl(mask) - 1);
+}
+
+/**
+ * mmio_write64_field() - Write value of 64-bit register field.
+ * See mmio_write32_field() for more details.
+ */
+static inline void mmio_write64_field(void *addr, u64 mask, u64 value)
+{
+	mmio_write64(addr, (mmio_read64(addr) & ~mask) |
+			((value << (__builtin_ffsl(mask) - 1)) & mask));
+}
+
+#endif /* !_JAILHOUSE_MMIO_H */
