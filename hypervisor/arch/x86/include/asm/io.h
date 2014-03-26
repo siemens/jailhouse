5dc803fce5aec09cb58a86a2c57d4261997a65fc
Ivan Kolchin <ivan.kolchin@siemens.com>
2014-03-26 11:24:43 +0400

    x86: Add 16/32-bit operations to input from/output to I/O port
    Operations inw/outw and inl/outl are added.
    
    Signed-off-by: Ivan Kolchin <ivan.kolchin@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/x86/include/asm/io.h
+++ hypervisor/arch/x86/include/asm/io.h
@@ -20,6 +20,33 @@ static inline void outb(u8 v, u16 port)
 static inline u8 inb(u16 port)
 {
 	u8 v;
+
 	asm volatile("inb %1,%0" : "=a" (v) : "dN" (port));
+	return v;
+}
+
+static inline void outw(u16 value, u16 port)
+{
+	asm volatile("outw %w0,%w1" : : "a" (value), "Nd" (port));
+}
+
+static inline u16 inw(u16 port)
+{
+	u16 v;
+
+	asm volatile("inw %w1,%0" : "=a" (v) : "Nd" (port));
+	return v;
+}
+
+static inline void outl(u32 value, u16 port)
+{
+	asm volatile("outl %0,%1" : : "a" (value), "Nd" (port));
+}
+
+static inline u32 inl(u16 port)
+{
+	u32 v;
+
+	asm volatile("inl %1,%0" : "=a" (v) : "dN" (port));
 	return v;
 }
