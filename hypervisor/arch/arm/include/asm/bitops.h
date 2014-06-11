5de9bf7fc93326d9697b235da7f26b4a9e570300
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2014-06-11 18:22:24 +0100

    arm: implement atomic bitops
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/arm/include/asm/bitops.h
+++ hypervisor/arch/arm/include/asm/bitops.h
@@ -15,41 +15,111 @@
 
 #include <jailhouse/types.h>
 
+#ifndef __ASSEMBLY__
+
+#define BITOPT_ALIGN(bits, addr)				\
+	do {							\
+		(addr) = (unsigned long *)((u32)(addr) & ~0x3)	\
+			+ (bits) / BITS_PER_LONG;		\
+		(bits) %= BITS_PER_LONG;			\
+	} while (0)
+
+/* Load the cacheline in exclusive state */
+#define PRELOAD(addr)						\
+	asm volatile (".arch_extension mp\n"			\
+		      "pldw %0\n"				\
+		      : "+Qo" (*(volatile unsigned long *)addr));
+
 static inline __attribute__((always_inline)) void
 clear_bit(int nr, volatile unsigned long *addr)
 {
+	unsigned long ret, val;
+
+	BITOPT_ALIGN(nr, addr);
+
+	PRELOAD(addr);
+	do {
+		asm volatile (
+		"ldrex	%1, %2\n"
+		"bic	%1, %3\n"
+		"strex	%0, %1, %2\n"
+		: "=r" (ret), "=r" (val),
+		/* Declare the clobbering of this address to the compiler */
+		  "+Qo" (*(volatile unsigned long *)addr)
+		:  "r" (1 << nr));
+	} while (ret);
 }
 
 static inline __attribute__((always_inline)) void
 set_bit(unsigned int nr, volatile unsigned long *addr)
 {
+	unsigned long ret, val;
+
+	BITOPT_ALIGN(nr, addr);
+
+	PRELOAD(addr);
+	do {
+		asm volatile (
+		"ldrex	%1, %2\n"
+		"orr	%1, %3\n"
+		"strex	%0, %1, %2\n"
+		: "=r" (ret), "=r" (val),
+		  "+Qo" (*(volatile unsigned long *)addr)
+		: "r" (1 << nr));
+	} while (ret);
 }
 
 static inline __attribute__((always_inline)) int
-constant_test_bit(unsigned int nr, const volatile unsigned long *addr)
+test_bit(unsigned int nr, const volatile unsigned long *addr)
 {
 	return ((1UL << (nr % BITS_PER_LONG)) &
 		(addr[nr / BITS_PER_LONG])) != 0;
 }
 
-static inline int variable_test_bit(int nr, volatile const unsigned long *addr)
-{
-	return 0;
-}
-
-#define test_bit(nr, addr)			\
-	(__builtin_constant_p((nr))		\
-	 ? constant_test_bit((nr), (addr))	\
-	 : variable_test_bit((nr), (addr)))
-
 static inline int test_and_set_bit(int nr, volatile unsigned long *addr)
 {
-	return 0;
+	unsigned long ret, val, test;
+
+	BITOPT_ALIGN(nr, addr);
+
+	PRELOAD(addr);
+	do {
+		asm volatile (
+		"ldrex	%1, %3\n"
+		"ands	%2, %1, %4\n"
+		"it	eq\n"
+		"orreq	%1, %4\n"
+		"strex	%0, %1, %3\n"
+		: "=r" (ret), "=r" (val), "=r" (test),
+		  "+Qo" (*(volatile unsigned long *)addr)
+		: "r" (1 << nr));
+	} while (ret);
+
+	return !!(test);
+}
+
+
+/* Count leading zeroes */
+static inline unsigned long clz(unsigned long word)
+{
+	unsigned long val;
+	asm volatile ("clz %0, %1\n" : "=r" (val) : "r" (word));
+	return val;
+}
+
+/* Returns the position of the least significant 1, MSB=31, LSB=0*/
+static inline unsigned long ffsl(unsigned long word)
+{
+	if (!word)
+		return 0;
+	asm volatile ("rbit %0, %0\n" : "+r" (word));
+	return clz(word);
 }
 
 static inline unsigned long ffzl(unsigned long word)
 {
-	return 0;
+	return ffsl(~word);
 }
 
+#endif /* !__ASSEMBLY__ */
 #endif /* !_JAILHOUSE_ASM_BITOPS_H */
