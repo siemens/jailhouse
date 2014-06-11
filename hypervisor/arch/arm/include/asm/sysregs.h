fd96ea09d7907a1e16902addb62310971c748c31
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2014-06-11 16:43:10 +0100

    arm: provide an interface for accessing system registers
    To avoid using inline asm directly in the setup and control code, to
    provide clear names for system registers, and to allow easier
    refactoring for a future arm64 port, this patch introduces some useful
    macros for accessing the core system registers.
    
    For a 64bit port, a couple of wrappers would still need to be added to
    modify system registers that are of different sizes on the two
    architectures (e.g. HCR)
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ hypervisor/arch/arm/include/asm/sysregs.h
@@ -0,0 +1,68 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _JAILHOUSE_ASM_SYSREGS_H
+#define _JAILHOUSE_ASM_SYSREGS_H
+
+/*
+ * Along with some system register names, this header defines the following
+ * macros for accessing cp15 registers.
+ *
+ * C-side:
+ * - arm_write_sysreg(SYSREG_NAME, var)
+ * - arm_read_sysreg(SYSREG_NAME, var)
+ * asm-side:
+ * - arm_write_sysreg(SYSREG_NAME, reg)
+ * - arm_read_sysreg(SYSREG_NAME, reg)
+ */
+
+
+#define SYSREG_32(...) 32, __VA_ARGS__
+#define SYSREG_64(...) 64, __VA_ARGS__
+
+#define _arm_write_sysreg(size, ...) arm_write_sysreg_ ## size(__VA_ARGS__)
+#define arm_write_sysreg(...) _arm_write_sysreg(__VA_ARGS__)
+
+#define _arm_read_sysreg(size, ...) arm_read_sysreg_ ## size(__VA_ARGS__)
+#define arm_read_sysreg(...) _arm_read_sysreg(__VA_ARGS__)
+
+#ifndef __ASSEMBLY__
+
+#define arm_write_sysreg_32(op1, crn, crm, op2, val) \
+	asm volatile ("mcr	p15, "#op1", %0, "#crn", "#crm", "#op2"\n" \
+			: : "r"((u32)(val)))
+#define arm_write_sysreg_64(op1, crm, val) \
+	asm volatile ("mcrr	p15, "#op1", %Q0, %R0, "#crm"\n" \
+			: : "r"((u64)(val)))
+
+#define arm_read_sysreg_32(op1, crn, crm, op2, val) \
+	asm volatile ("mrc	p15, "#op1", %0, "#crn", "#crm", "#op2"\n" \
+			: "=r"((u32)(val)))
+#define arm_read_sysreg_64(op1, crm, val) \
+	asm volatile ("mrrc	p15, "#op1", %Q0, %R0, "#crm"\n" \
+			: "=r"((u64)(val)))
+
+#else /* __ASSEMBLY__ */
+
+#define arm_write_sysreg_32(op1, crn, crm, op2, reg) \
+	mcr	p15, op1, reg, crn, crm, op2
+#define arm_write_sysreg_64(op1, crm, reg1, reg2) \
+	mcrr	p15, op1, reg1, reg2, crm
+
+#define arm_read_sysreg_32(op1, crn, crm, op2, reg) \
+	mrc	p15, op1, reg, crn, crm, op2
+#define arm_read_sysreg_64(op1, crm, reg1, reg2) \
+	mrrc	p15, op1, reg1, reg2, crm
+
+#endif /* __ASSEMBLY__ */
+
+#endif
