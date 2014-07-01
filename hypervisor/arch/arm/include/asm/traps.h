e47548aab160c95d2bd07b3f578e6f61b2d01394
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2014-07-01 19:08:28 +0100

    arm: read/write the banked registers
    When emulating instructions, the trap handler will need to access the
    cell registers according to the guest's processor mode when the trap
    occurred, which is stored inside the saved PSR.
    This patch allows to directly read and write the banked registers.
    If HSR reports a load into r14 and the mode was IRQ for instance, the
    hypervisor will need to write something into LR_IRQ instead of the LR
    saved on the stack.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/arm/include/asm/traps.h
+++ hypervisor/arch/arm/include/asm/traps.h
@@ -13,6 +13,7 @@
 #ifndef _JAILHOUSE_ASM_TRAPS_H
 #define _JAILHOUSE_ASM_TRAPS_H
 
+#include <jailhouse/printk.h>
 #include <jailhouse/types.h>
 #include <asm/head.h>
 #include <asm/percpu.h>
@@ -28,6 +29,7 @@ struct trap_context {
 	unsigned long *regs;
 	u32 esr;
 	u32 cpsr;
+	u32 pc;
 };
 
 typedef int (*trap_handler)(struct per_cpu *cpu_data,
@@ -38,6 +40,51 @@ typedef int (*trap_handler)(struct per_cpu *cpu_data,
 
 #define arm_write_banked_reg(reg, val)					\
 	asm volatile ("msr " #reg ", %0\n" : : "r" (val))
+
+#define _access_banked(reg, val, is_read)				\
+	do {								\
+		if (is_read)						\
+			arm_write_banked_reg(reg, val);			\
+		else							\
+			arm_read_banked_reg(reg, val);			\
+	} while (0)
+
+#define access_banked_reg(mode, reg, val, is_read)			\
+	do {								\
+		switch (reg) {						\
+		case 13:						\
+			_access_banked(SP_##mode, *val, is_read);	\
+			break;						\
+		case 14:						\
+			_access_banked(LR_##mode, *val, is_read);	\
+			break;						\
+		default:						\
+			printk("ERROR: access r%d in "#mode"\n", reg);	\
+		}							\
+	} while (0)
+
+static inline void access_fiq_reg(u8 reg, unsigned long *val, bool is_read)
+{
+	switch (reg) {
+	case 8:  _access_banked(r8_fiq,  *val, is_read); break;
+	case 9:  _access_banked(r9_fiq,  *val, is_read); break;
+	case 10: _access_banked(r10_fiq, *val, is_read); break;
+	case 11: _access_banked(r11_fiq, *val, is_read); break;
+	case 12: _access_banked(r12_fiq, *val, is_read); break;
+	default:
+		 /* Use existing error reporting */
+		 access_banked_reg(fiq, reg, val, is_read);
+	}
+}
+
+static inline void access_usr_reg(struct trap_context *ctx, u8 reg,
+				  unsigned long *val, bool is_read)
+{
+	if (is_read)
+		*val = ctx->regs[reg];
+	else
+		ctx->regs[reg] = *val;
+}
 
 #endif /* !__ASSEMBLY__ */
 #endif /* !_JAILHOUSE_ASM_TRAPS_H */
--- hypervisor/arch/arm/traps.c
+++ hypervisor/arch/arm/traps.c
@@ -16,6 +16,71 @@
 #include <jailhouse/printk.h>
 #include <jailhouse/control.h>
 
+static void access_cell_reg(struct trap_context *ctx, u8 reg,
+				unsigned long *val, bool is_read)
+{
+	unsigned long mode = ctx->cpsr & PSR_MODE_MASK;
+
+	switch (reg) {
+	case 0 ... 7:
+		access_usr_reg(ctx, reg, val, is_read);
+		break;
+	case 8 ... 12:
+		if (mode == PSR_FIQ_MODE)
+			access_fiq_reg(reg, val, is_read);
+		else
+			access_usr_reg(ctx, reg, val, is_read);
+		break;
+	case 13 ... 14:
+		switch (mode) {
+		case PSR_USR_MODE:
+		case PSR_SYS_MODE:
+			/*
+			 * lr is saved on the stack, as it is not banked in HYP
+			 * mode. sp is banked, so lr is at offset 13 in the USR
+			 * regs.
+			 */
+			if (reg == 13)
+				access_banked_reg(usr, reg, val, is_read);
+			else
+				access_usr_reg(ctx, 13, val, is_read);
+			break;
+		case PSR_SVC_MODE:
+			access_banked_reg(svc, reg, val, is_read);
+			break;
+		case PSR_UND_MODE:
+			access_banked_reg(und, reg, val, is_read);
+			break;
+		case PSR_ABT_MODE:
+			access_banked_reg(abt, reg, val, is_read);
+			break;
+		case PSR_IRQ_MODE:
+			access_banked_reg(irq, reg, val, is_read);
+			break;
+		case PSR_FIQ_MODE:
+			access_banked_reg(fiq, reg, val, is_read);
+			break;
+		}
+		break;
+	case 15:
+		/*
+		 * A trapped instruction that accesses the PC? Probably a bug,
+		 * but nothing seems to prevent it.
+		 */
+		printk("WARNING: trapped instruction attempted to explicitly "
+		       "access the PC.\n");
+		if (is_read)
+			*val = ctx->pc;
+		else
+			ctx->pc = *val;
+		break;
+	default:
+		/* Programming error */
+		printk("ERROR: attempt to write register %d\n", reg);
+		break;
+	}
+}
+
 static int arch_handle_hvc(struct per_cpu *cpu_data, struct trap_context *ctx)
 {
 	unsigned long *regs = ctx->regs;
@@ -36,6 +101,7 @@ void arch_handle_trap(struct per_cpu *cpu_data, struct registers *guest_regs)
 	u32 exception_class;
 	int ret = TRAP_UNHANDLED;
 
+	arm_read_banked_reg(ELR_hyp, ctx.pc);
 	arm_read_banked_reg(SPSR_hyp, ctx.cpsr);
 	arm_read_sysreg(ESR_EL2, ctx.esr);
 	exception_class = ESR_EC(ctx.esr);
@@ -49,4 +115,6 @@ void arch_handle_trap(struct per_cpu *cpu_data, struct registers *guest_regs)
 				cpu_data->cpu_id, ctx.esr);
 		while(1);
 	}
+
+	arm_write_banked_reg(ELR_hyp, ctx.pc);
 }
