cbe49fcf81485b9c236da2dda2aff13b6ba5e8af
Jan Kiszka <jan.kiszka@siemens.com>
2014-07-17 20:28:54 +0200

    inmates: Provide missing PIO accessors
    Add inw, outw and outl.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- inmates/lib/x86/inmate.h
+++ inmates/lib/x86/inmate.h
@@ -69,10 +69,27 @@ static inline void outb(u8 v, u16 port)
 	asm volatile("outb %0,%1" : : "a" (v), "dN" (port));
 }
 
+static inline void outw(u16 v, u16 port)
+{
+	asm volatile("outw %0,%1" : : "a" (v), "dN" (port));
+}
+
+static inline void outl(u32 v, u16 port)
+{
+	asm volatile("outl %0,%1" : : "a" (v), "dN" (port));
+}
+
 static inline u8 inb(u16 port)
 {
 	u8 v;
 	asm volatile("inb %1,%0" : "=a" (v) : "dN" (port));
+	return v;
+}
+
+static inline u16 inw(u16 port)
+{
+	u16 v;
+	asm volatile("inw %1,%0" : "=a" (v) : "dN" (port));
 	return v;
 }
 
