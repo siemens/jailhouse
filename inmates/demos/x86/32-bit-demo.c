e477b960d6c5c3217005a96b1eab21ffe4a34d3c
Jan Kiszka <jan.kiszka@siemens.com>
2014-07-17 21:24:58 +0200

    inmates: Add primitive 32-bit demo
    This only demonstrates the capability to build inmates which come up in
    32-bit mode. Note that not all library services support this already.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ inmates/demos/x86/32-bit-demo.c
@@ -0,0 +1,30 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2014
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ *
+ * Use with tiny-demo config e.g.
+ */
+
+#include <inmate.h>
+
+#ifdef CONFIG_UART_OXPCIE952
+#define UART_BASE		0xe000
+#else
+#define UART_BASE		0x2f8
+#endif
+
+#define IA32_EFER	0xc0000080
+
+void inmate_main(void)
+{
+	printk_uart_base = UART_BASE;
+	printk("This runs in 32-bit mode (EFER: %x)\n", read_msr(IA32_EFER));
+}
