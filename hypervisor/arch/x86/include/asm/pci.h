1187012d7acda26a9a67efc8b02d6e3eb517fb26
Jan Kiszka <jan.kiszka@siemens.com>
2014-07-28 07:45:04 +0200

    x86: Clean up PCI defines
    Most of them became unused. Use self-describing definitions for the
    remaining ones.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/x86/include/asm/pci.h
+++ hypervisor/arch/x86/include/asm/pci.h
@@ -5,6 +5,7 @@
  *
  * Authors:
  *  Ivan Kolchin <ivan.kolchin@siemens.com>
+ *  Jan Kiszka <jan.kiszka@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -21,20 +22,9 @@
 #define PCI_REG_DATA_PORT		0xcfc
 
 /* --- Address register fields --- */
-/* Bits 31: Enable bit*/
-#define PCI_ADDR_ENABLE			0x80000000
-/* Bits 23-16: Bus number */
-#define PCI_ADDR_BUS_MASK		0x00ff0000
-/* Bits 15-11: Device number */
-#define PCI_ADDR_DEV_MASK		0x0000f800
-/* Bits 10-8: Function number */
-#define PCI_ADDR_FUNC_MASK		0x00000700
-/* Bits 7-2: Register number */
-#define PCI_ADDR_REGNUM_MASK		0x000000fc
-#define PCI_ADDR_VALID_MASK \
-	(PCI_ADDR_ENABLE | PCI_ADDR_BUS_MASK | PCI_ADDR_DEV_MASK | \
-	 PCI_ADDR_FUNC_MASK | PCI_ADDR_REGNUM_MASK)
+#define PCI_ADDR_ENABLE			(1UL << 31)
 #define PCI_ADDR_BDF_SHIFT		8
+#define PCI_ADDR_REGNUM_MASK		BIT_MASK(7, 2)
 
 int x86_pci_config_handler(struct registers *guest_regs, struct cell *cell,
 			   u16 port, bool dir_in, unsigned int size);
