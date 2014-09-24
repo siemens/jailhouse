89b07159e939688c83a04fdac0fdd476518d0abd
Jan Kiszka <jan.kiszka@siemens.com>
2014-09-24 21:13:54 +0200

    x86: Document cell structure
    Document the x86-specific cell structure. This is a temporary solution
    until we factored out generic bits.
    
    The cell structure is cross-subsystem, so leave it unassigned to
    group.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/x86/include/asm/cell.h
+++ hypervisor/arch/x86/include/asm/cell.h
@@ -20,49 +20,67 @@
 
 struct pci_device;
 
-/**
- * struct cell - cell-related state information
- * ...
- * @pci_addr_port_val: virtual address port for PCI config space
- * ...
- */
+/** Cell-related states. */
 /* TODO: factor out arch-independent bits, define struct arch_cell */
 struct cell {
 	struct {
+		/** PIO access bitmap. */
 		/* should be first as it requires page alignment */
 		u8 __attribute__((aligned(PAGE_SIZE))) io_bitmap[2*PAGE_SIZE];
+		/** Paging structures used for cell CPUs. */
 		struct paging_structures ept_structs;
-	} vmx;
+	} vmx; /**< Intel VMX-specific fields. */
 
 	struct {
+		/** Paging structures used for DMA requests. */
 		struct paging_structures pg_structs;
+		/** True if interrupt remapping support is emulated for this
+		 * cell. */
 		bool ir_emulation;
-	} vtd;
+	} vtd; /**< Inte VT-d specific fields. */
 
+	/** ID of the cell. */
 	unsigned int id;
+	/** Number of pages used for storing cell-specific states and
+	 * configuration data. */
 	unsigned int data_pages;
+	/** Pointer to static cell description. */
 	struct jailhouse_cell_desc *config;
 
+	/** Pointer to cell's CPU set. */
 	struct cpu_set *cpu_set;
+	/** Stores the cell's CPU set if small enough. */
 	struct cpu_set small_cpu_set;
 
+	/** True while the cell can be loaded by the root cell. */
 	bool loadable;
 
+	/** Pointer to next cell in the system. */
 	struct cell *next;
 
+	/** List of PCI devices assigned to this cell. */
 	struct pci_device *pci_devices;
+	/** List of PCI devices assigned to this cell that support MSI-X. */
 	struct pci_device *msix_device_list;
+	/** Shadow value of PCI config space address port register. */
 	u32 pci_addr_port_val;
 
+	/** Shadow value of IOAPIC index register. */
 	u32 ioapic_index_reg_val;
+	/** IOAPIC ID as used for interrupt remapping. */
 	u16 ioapic_id;
+	/** IOMMU unit responsible for the IOAPIC. */
 	u8 ioapic_iommu;
+	/** Bitmap of IOAPIC pins assigned to this cell. */
 	u64 ioapic_pin_bitmap;
 
 	union {
+		/** Communication region. */
 		struct jailhouse_comm_region comm_region;
+		/** Padding to full page size. */
 		u8 padding[PAGE_SIZE];
 	} __attribute__((aligned(PAGE_SIZE))) comm_page;
+	/**< Page containing the communication region (shared with cell). */
 };
 
 extern struct cell root_cell;
