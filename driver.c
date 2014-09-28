3db48bd55cb7bc3eece6b3ed5bf6010806834269
Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
2014-09-28 22:42:36 +0600

    driver: Support "variants" in firmware name
    Jailhouse loader was also updated to locate and load a correct firmware
    binary depending on a type of the system hypervisor executes on.
    
    Signed-off-by: Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
    [Jan: adjusted #endif to keep MODULE_VERSION unconditional]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- driver.c
+++ driver.c
@@ -2,9 +2,11 @@
  * Jailhouse, a Linux-based partitioning hypervisor
  *
  * Copyright (c) Siemens AG, 2013, 2014
+ * Copyright (c) Valentine Sinitsyn, 2014
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
+ *  Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -82,7 +84,12 @@ static const struct sysfs_ops cell_sysfs_ops = {
 #endif /* < 3.14 */
 /* End of compatibility section - remove as version become obsolete */
 
+#ifdef CONFIG_X86
+#define JAILHOUSE_AMD_FW_NAME	"jailhouse-amd.bin"
+#define JAILHOUSE_INTEL_FW_NAME	"jailhouse-intel.bin"
+#else
 #define JAILHOUSE_FW_NAME	"jailhouse.bin"
+#endif
 
 struct cell {
 	struct kobject kobj;
@@ -95,7 +102,12 @@ struct cell {
 
 MODULE_DESCRIPTION("Loader for Jailhouse partitioning hypervisor");
 MODULE_LICENSE("GPL");
+#ifdef CONFIG_X86
+MODULE_FIRMWARE(JAILHOUSE_AMD_FW_NAME);
+MODULE_FIRMWARE(JAILHOUSE_INTEL_FW_NAME);
+#else
 MODULE_FIRMWARE(JAILHOUSE_FW_NAME);
+#endif
 MODULE_VERSION(JAILHOUSE_VERSION);
 
 static struct device *jailhouse_dev;
@@ -396,6 +408,19 @@ static void enter_hypervisor(void *info)
 	atomic_inc(&call_done);
 }
 
+static inline const char * jailhouse_fw_name(void)
+{
+#ifdef CONFIG_X86
+	if (boot_cpu_has(X86_FEATURE_SVM))
+		return JAILHOUSE_AMD_FW_NAME;
+	if (boot_cpu_has(X86_FEATURE_VMX))
+		return JAILHOUSE_INTEL_FW_NAME;
+	return NULL;
+#else
+	return JAILHOUSE_FW_NAME;
+#endif
+}
+
 static int jailhouse_enable(struct jailhouse_system __user *arg)
 {
 	const struct firmware *hypervisor;
@@ -404,8 +429,15 @@ static int jailhouse_enable(struct jailhouse_system __user *arg)
 	struct jailhouse_memory *hv_mem = &config_header.hypervisor_memory;
 	struct jailhouse_header *header;
 	unsigned long config_size;
+	const char *fw_name;
 	long max_cpus;
 	int err;
+
+	fw_name = jailhouse_fw_name();
+	if (!fw_name) {
+		pr_err("jailhouse: Missing or unsupported HVM technology\n");
+		return -ENODEV;
+	}
 
 	if (copy_from_user(&config_header, arg, sizeof(config_header)))
 		return -EFAULT;
@@ -424,10 +456,9 @@ static int jailhouse_enable(struct jailhouse_system __user *arg)
 	if (enabled || !try_module_get(THIS_MODULE))
 		goto error_unlock;
 
-	err = request_firmware(&hypervisor, JAILHOUSE_FW_NAME, jailhouse_dev);
+	err = request_firmware(&hypervisor, fw_name, jailhouse_dev);
 	if (err) {
-		pr_err("jailhouse: Missing hypervisor image %s\n",
-		       JAILHOUSE_FW_NAME);
+		pr_err("jailhouse: Missing hypervisor image %s\n", fw_name);
 		goto error_put_module;
 	}
 
