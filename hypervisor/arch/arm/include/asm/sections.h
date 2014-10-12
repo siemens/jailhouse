36b50ce2c3b5b0dfa2e3632356655c75d0eb5035
Jan Kiszka <jan.kiszka@siemens.com>
2014-10-12 15:54:05 +0200

    core: Refactor arch-specific section definition
    Require all archs to define ARCH_SECTIONS via asm/section.h, at least an
    empty one. Include this unconditionally in the hypervisor layout.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/arm/include/asm/sections.h
+++ hypervisor/arch/arm/include/asm/sections.h
@@ -2,17 +2,20 @@
  * Jailhouse, a Linux-based partitioning hypervisor
  *
  * Copyright (c) ARM Limited, 2014
+ * Copyright (c) Siemens AG, 2014
  *
  * Authors:
  *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *  Jan Kiszka <jan.kiszka@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
-. = ALIGN(0x1000);
-.trampoline : {
-		trampoline_start = .;
-		*(.trampoline)
-		trampoline_end = .;
-}
+#define ARCH_SECTIONS							\
+	. = ALIGN(PAGE_SIZE);						\
+	.trampoline : {							\
+		trampoline_start = .;					\
+		*(.trampoline)						\
+		trampoline_end = .;					\
+	}
---
+++ hypervisor/arch/x86/include/asm/sections.h
@@ -0,0 +1,14 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2014
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+/* no special sections */
+#define ARCH_SECTIONS
