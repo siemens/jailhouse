63e7b331acbabccbdcc846e159a1a18b493e3e56
Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
2014-11-10 16:41:04 +0500

    core: Add asm-defines.h generation
    Adapted Linux kernel tools to generate asm-offset.h for Jailhouse.
    This is meant to generate common defines (C structure member offsets
    and other compile-time expressions) in a way accessible from the
    assembly code.
    
    Signed-off-by: Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
    [Jan: moved to include/generated, added copyright headers, fix typo]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ hypervisor/arch/x86/asm-defines.c
@@ -0,0 +1,19 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Valentine Sinitsyn, 2014
+ *
+ * Authors:
+ *  Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/gen-defines.h>
+
+void common(void);
+
+void common(void)
+{
+}
---
+++ hypervisor/include/jailhouse/gen-defines.h
@@ -0,0 +1,27 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Valentine Sinitsyn, 2014
+ *
+ * Authors:
+ *  Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _JAILHOUSE_GEN_DEFINES_H
+#define _JAILHOUSE_GEN_DEFINES_H
+
+#define DEFINE(sym, val) \
+        asm volatile("\n=>" #sym " %0 " #val : : "i" (val))
+
+#define BLANK() asm volatile("\n=>" : : )
+
+#define OFFSET(sym, str, mem) \
+	DEFINE(sym, __builtin_offsetof(struct str, mem))
+
+#define COMMENT(x) \
+	asm volatile("\n=>#" x)
+
+#endif
