ffef8d9ddf519b5a1f3a7b15b8470125207d2005
Jan Kiszka <jan.kiszka@siemens.com>
2014-11-12 20:41:13 +0100

    core: Introduce FIELD_SIZEOF
    Will be used for asm-defines soon.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/include/jailhouse/utils.h
+++ hypervisor/include/jailhouse/utils.h
@@ -1,16 +1,21 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013
+ * Copyright (c) Siemens AG, 2014
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
+ *
+ * Partly derived from Linux kernel code.
  */
 
 #define ARRAY_SIZE(array)	(sizeof(array) / sizeof((array)[0]))
+
+/* sizeof() for a structure/union field */
+#define FIELD_SIZEOF(type, fld)	(sizeof(((type *)0)->fld))
 
 /* create 64-bit mask with bytes 0 to size-1 set to 0xff */
 #define BYTE_MASK(size)		(0xffffffffffffffffULL >> ((8 - (size)) * 8))
