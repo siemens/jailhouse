a87b30d947fab872b3980e7c970e70fec9b1b031
Henning Schild <henning.schild@siemens.com>
2014-11-24 18:54:17 +0100

    inmates, configs: add ivshmem virtual PCI device demo code and config
    New demo to demonstrate ivshmem virtual PCI devices for shared memory
    inter-cell communication.
    
    Signed-off-by: Henning Schild <henning.schild@siemens.com>
    [Jan: adjusted region location]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ configs/ivshmem-demo.c
@@ -0,0 +1,81 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2013, 2014
+ *
+ * Authors:
+ *  Henning Schild <henning.schild@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <linux/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[3];
+	__u8 pio_bitmap[0x2000];
+	struct jailhouse_pci_device pci_devices[1];
+	struct jailhouse_pci_capability pci_caps[0];
+} __attribute__((packed)) config = {
+	.cell = {
+		.name = "ivshmem-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = 0,
+		.pio_bitmap_size = ARRAY_SIZE(config.pio_bitmap),
+		.num_pci_devices = ARRAY_SIZE(config.pci_devices),
+		.num_pci_caps = ARRAY_SIZE(config.pci_caps),
+	},
+
+	.cpus = {
+		0b0100,
+	},
+
+	.mem_regions = {
+		/* RAM */ {
+			.phys_start = 0x3f000000,
+			.virt_start = 0,
+			.size = 0x00100000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* communication region */ {
+			.virt_start = 0x00100000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_COMM_REGION,
+		},
+		/* IVSHMEM shared memory region */
+		{
+			.phys_start = 0x3f1ff000,
+			.virt_start = 0x3f1ff000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_ROOTSHARED,
+		},
+	},
+
+	.pio_bitmap = {
+		[     0/8 ...  0x3f7/8] = -1,
+		[ 0x3f8/8 ...  0x3ff/8] = 0, /* serial1 */
+		[ 0x400/8 ... 0xffff/8] = -1,
+	},
+
+	.pci_devices = {
+		{
+			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
+			.domain = 0x0,
+			.bdf = (0x0f<<3),
+			.shmem_region = 2,
+			.num_msix_vectors = 1,
+		},
+	},
+};
