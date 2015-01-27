dce9fb4b7245085de5e776d9604fcc7624f9b027
Jan Kiszka <jan.kiszka@siemens.com>
2015-01-27 11:09:21 +0100

    x86: Switch to ticket spinlocks
    ARM already has it, x86 should gain it as well: To avoid the risk of
    unfair lock assignment or even starvation in excessive contention
    scenarios, switch to the ticket-based spinlock algorithm that also Linux
    uses. Our implementation is a condensed version of the kernel as we do
    not have to take para-virtual optimizations and instrumentations into
    account.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/x86/include/asm/spinlock.h
+++ hypervisor/arch/x86/include/asm/spinlock.h
@@ -17,21 +17,32 @@
 #include <asm/processor.h>
 
 typedef struct {
-	unsigned long state;
+	u16 owner, next;
 } spinlock_t;
 
 #define DEFINE_SPINLOCK(name)	spinlock_t (name)
 
 static inline void spin_lock(spinlock_t *lock)
 {
-	while (test_and_set_bit(0, &lock->state))
-		cpu_relax();
+	register spinlock_t inc = { .next = 1 };
+
+	asm volatile("lock xaddl %0, %1"
+		: "+r" (inc), "+m" (*lock)
+		: : "memory", "cc");
+
+	if (inc.owner != inc.next)
+		while (lock->owner != inc.next)
+			cpu_relax();
+
+	asm volatile("" : : : "memory");
 }
 
 static inline void spin_unlock(spinlock_t *lock)
 {
-	asm volatile("" : : : "memory");
-	clear_bit(0, &lock->state);
+	asm volatile("addw %1, %0"
+		: "+m" (lock->owner)
+		: "ri" (1)
+		: "memory", "cc");
 }
 
 #endif /* !_JAILHOUSE_ASM_SPINLOCK_H */
