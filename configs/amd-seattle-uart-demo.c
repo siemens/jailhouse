5cba8f7e3d501b3462bfedeb1323eeb51ac26e43
Antonios Motakis <antonios.motakis@huawei.com>
2016-05-12 15:01:00 +0200

    config: UART demo cell config for the AMD Seattle
    Add a cell configuration file for the AMD Seattle development
    board, to be used with the PL011 UART demo inmate.
    
    Signed-off-by: Antonios Motakis <antonios.motakis@huawei.com>
    [Jan: adjust include]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ configs/amd-seattle-uart-demo.c
@@ -0,0 +1,55 @@
+/*
+ * Jailhouse AArch64 support
+ *
+ * Copyright (C) 2015 Huawei Technologies Duesseldorf GmbH
+ *
+ * Authors:
+ *  Antonios Motakis <antonios.motakis@huawei.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[2];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.name = "pl011-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = 0,
+		.pio_bitmap_size = 0,
+		.num_pci_devices = 0,
+	},
+
+	.cpus = {
+		0x4,
+	},
+
+	.mem_regions = {
+		/* UART 2 */ {
+			.phys_start = 0xe1010000,
+			.virt_start = 0xe1010000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* RAM */ {
+			.phys_start = 0x82fbff0000,
+			.virt_start = 0,
+			.size = 0x00010000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+	}
+};
