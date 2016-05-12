81ccd0f2773fd7bb6ca01e054afa1dcf33cc0be2
Antonios Motakis <antonios.motakis@huawei.com>
2016-05-12 15:00:59 +0200

    arm64: add cell.h header file
    Add the initial cell.h header file needed to build on AArch64.
    
    Signed-off-by: Antonios Motakis <antonios.motakis@huawei.com>
    [Jan: remove obsolete fields and includes]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ hypervisor/arch/arm64/include/asm/cell.h
@@ -0,0 +1,25 @@
+/*
+ * Jailhouse AArch64 support
+ *
+ * Copyright (C) 2015 Huawei Technologies Duesseldorf GmbH
+ *
+ * Authors:
+ *  Antonios Motakis <antonios.motakis@huawei.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#ifndef _JAILHOUSE_ASM_CELL_H
+#define _JAILHOUSE_ASM_CELL_H
+
+#ifndef __ASSEMBLY__
+
+#include <jailhouse/paging.h>
+
+struct arch_cell {
+	struct paging_structures mm;
+};
+
+#endif /* !__ASSEMBLY__ */
+#endif /* !_JAILHOUSE_ASM_CELL_H */
