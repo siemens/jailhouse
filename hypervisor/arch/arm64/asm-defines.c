067af81d5c52ad8cb6293e6c60eb11fdbeed854a
Jan Kiszka <jan.kiszka@siemens.com>
2016-05-12 15:01:00 +0200

    arm64: Implement data cache flush
    The arm_dcaches_flush function is needed early on during initialization,
    in order to flush the early bootstrap page tables for they hypervisor.
    
    Based on patch by Antonios Motakis.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/arm64/asm-defines.c
+++ hypervisor/arch/arm64/asm-defines.c
@@ -2,18 +2,24 @@
  * Jailhouse AArch64 support
  *
  * Copyright (C) 2015 Huawei Technologies Duesseldorf GmbH
+ * Copyright (c) Siemens AG, 2016
  *
  * Authors:
  *  Antonios Motakis <antonios.motakis@huawei.com>
+ *  Jan Kiszka <jan.kiszka@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
+#include <jailhouse/paging.h>
 #include <jailhouse/gen-defines.h>
 
 void common(void);
 
 void common(void)
 {
+	DEFINE(DCACHE_CLEAN_ASM, DCACHE_CLEAN);
+	DEFINE(DCACHE_INVALIDATE_ASM, DCACHE_INVALIDATE);
+	DEFINE(DCACHE_CLEAN_AND_INVALIDATE_ASM, DCACHE_CLEAN_AND_INVALIDATE);
 }
