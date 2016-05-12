b194f27995a813a58d4315d2aced748f2a253fd9
Antonios Motakis <antonios.motakis@huawei.com>
2016-05-12 15:01:01 +0200

    config: add linux inmate cell config for AMD Seattle
    Add the cell configuration files, and some helper scripts and device
    tree for the AMD Seattle development board. These can be used to
    load a linux inmate on a cell on that target.
    
    Signed-off-by: Antonios Motakis <antonios.motakis@huawei.com>
    [Jan: adjust to new irqchip configuration, adjust include]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ configs/amd-seattle-linux-demo.c
@@ -0,0 +1,100 @@
+/*
+ * Jailhouse AArch64 support
+ *
+ * Copyright (C) 2015 Huawei Technologies Duesseldorf GmbH
+ *
+ * Authors:
+ *  Antonios Motakis <antonios.motakis@huawei.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[6];
+	struct jailhouse_irqchip irqchips[1];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.name = "linux-inmate-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = 1,
+		.pio_bitmap_size = 0,
+		.num_pci_devices = 0,
+	},
+
+	.cpus = {
+		0xc0,
+	},
+
+	.mem_regions = {
+		/* UART */ {
+			.phys_start = 0xe1010000,
+			.virt_start = 0xe1010000,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* xgmac */ {
+			.phys_start = 0xe0900000,
+			.virt_start = 0xe0900000,
+			.size =         0x100000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* phy */ {
+			.phys_start = 0xe1240000,
+			.virt_start = 0xe1240000,
+			.size =           0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* phy */ {
+			.phys_start = 0xe1250000,
+			.virt_start = 0xe1250000,
+			.size =           0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* RAM */ {
+			.phys_start = 0x82d0000000,
+			.virt_start = 0x0,
+			.size = 0x10000000,	/* 256 MiB */
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* RAM */ {
+			.phys_start = 0x82e0000000,
+			.virt_start = 0x82e0000000,
+			.size =         0x10000000,	/* 256 MiB */
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+	},
+
+	.irqchips = {
+		/* GIC */ {
+			.address = 0xe1110000,
+			.pin_base = 352,
+			.pin_bitmap = {
+				(1 << (354 - 352)) | /* xgmac1 */
+				(1 << (356 - 352)) | /* xgmac1 */
+				(1 << (360 - 352)) | /* uart */
+				(1 << (373 - 352)) | /* xgmac1 */
+				(1 << (374 - 352)) | /* xgmac1 */
+				(1 << (375 - 352)) | /* xgmac1 */
+				(1 << (376 - 352))   /* xgmac1 */
+			},
+		},
+	}
+};
