00ec69455399f48b15729b4275c406ed11083fd8
Jan Kiszka <jan.kiszka@siemens.com>
2016-08-15 10:27:30 +0200

    inmates: arm: Add non-root Linux loader
    This adds a minimalistic boot loader for Linux non-root cells. It will
    be used by the loader script later on which will figure out the right
    locations for kernel and device tree and pass their addresses to to the
    inmate command line.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ inmates/tools/arm/linux-loader.c
@@ -0,0 +1,29 @@
+/*
+ * Jailhouse ARM support
+ *
+ * Copyright (C) 2015 Huawei Technologies Duesseldorf GmbH
+ * Copyright (c) Siemens AG, 2016
+ *
+ * Authors:
+ *  Dmitry Voytik <dmitry.voytik@huawei.com>
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <inmate.h>
+
+#define CMDLINE_BUFFER_SIZE	256
+CMDLINE_BUFFER(CMDLINE_BUFFER_SIZE);
+
+void inmate_main(void)
+{
+	void register (*entry)(unsigned long, unsigned long, unsigned long);
+	unsigned long register dtb;
+
+	entry = (void *)(unsigned long)cmdline_parse_int("kernel", 0);
+	dtb = cmdline_parse_int("dtb", 0);
+
+	entry(0, -1, dtb);
+}
