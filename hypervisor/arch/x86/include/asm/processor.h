siemens/jailhouse

commit 10c6aac6eff45d11c635d3c9730288bdb01dcf33

x86: Always intercept cpuid
Refactor vmx_handle_cpuid to vcpu_handle_cpuid and ensure that both VMX
and SVM use it for emulating guest cpuid invocations. That means SVM has
to intercept it now.

We will need this to reliably indicate the presence of Jailhouse to our
inmates.

CC: Valentine Sinitsyn <valentine.sinitsyn@gmail.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/vcpu.c
@@ -311,6 +311,24 @@ bool vcpu_handle_msr_write(void)
 	return true;
 }
 
+void vcpu_handle_cpuid(void)
+{
+	union registers *guest_regs = &this_cpu_data()->guest_regs;
+
+	this_cpu_data()->stats[JAILHOUSE_CPU_STAT_VMEXITS_CPUID]++;
+
+	/* clear upper 32 bits of the involved registers */
+	guest_regs->rax &= 0xffffffff;
+	guest_regs->rbx &= 0xffffffff;
+	guest_regs->rcx &= 0xffffffff;
+	guest_regs->rdx &= 0xffffffff;
+
+	cpuid((u32 *)&guest_regs->rax, (u32 *)&guest_regs->rbx,
+	      (u32 *)&guest_regs->rcx, (u32 *)&guest_regs->rdx);
+
+	vcpu_skip_emulated_instruction(X86_INST_LEN_CPUID);
+}
+
 bool vcpu_handle_xsetbv(void)
 {
 	union registers *guest_regs = &this_cpu_data()->guest_regs;

hypervisor/arch/x86/include/asm/vcpu.h
@@ -113,6 +113,8 @@ bool vcpu_handle_mmio_access(void);
 bool vcpu_handle_msr_read(void);
 bool vcpu_handle_msr_write(void);
 
+void vcpu_handle_cpuid(void);
+
 bool vcpu_handle_xsetbv(void);
 
 void vcpu_reset(bool hard_reset);

hypervisor/arch/x86/svm.c
@@ -199,6 +199,7 @@ static void vmcb_setup(struct per_cpu *cpu_data)
 
 	vmcb->general1_intercepts |= GENERAL1_INTERCEPT_NMI;
 	vmcb->general1_intercepts |= GENERAL1_INTERCEPT_CR0_SEL_WRITE;
+	vmcb->general1_intercepts |= GENERAL1_INTERCEPT_CPUID;
 	vmcb->general1_intercepts |= GENERAL1_INTERCEPT_IOIO_PROT;
 	vmcb->general1_intercepts |= GENERAL1_INTERCEPT_MSR_PROT;
 	vmcb->general1_intercepts |= GENERAL1_INTERCEPT_SHUTDOWN_EVT;
@@ -879,6 +880,9 @@ void vcpu_handle_exit(struct per_cpu *cpu_data)
 		if (svm_handle_cr(cpu_data))
 			goto vmentry;
 		break;
+	case VMEXIT_CPUID:
+		vcpu_handle_cpuid();
+		goto vmentry;
 	case VMEXIT_MSR:
 		cpu_data->stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR]++;
 		if (!vmcb->exitinfo1)

hypervisor/arch/x86/vmx.c
@@ -881,22 +881,6 @@ void vcpu_skip_emulated_instruction(unsigned int inst_len)
 	vmcs_write64(GUEST_RIP, vmcs_read64(GUEST_RIP) + inst_len);
 }
 
-static void vmx_handle_cpuid(union registers *guest_regs)
-{
-	this_cpu_data()->stats[JAILHOUSE_CPU_STAT_VMEXITS_CPUID]++;
-
-	/* clear upper 32 bits of the involved registers */
-	guest_regs->rax &= 0xffffffff;
-	guest_regs->rbx &= 0xffffffff;
-	guest_regs->rcx &= 0xffffffff;
-	guest_regs->rdx &= 0xffffffff;
-
-	cpuid((u32 *)&guest_regs->rax, (u32 *)&guest_regs->rbx,
-	      (u32 *)&guest_regs->rcx, (u32 *)&guest_regs->rdx);
-
-	vcpu_skip_emulated_instruction(X86_INST_LEN_CPUID);
-}
-
 static void update_efer(void)
 {
 	unsigned long efer = vmcs_read64(GUEST_IA32_EFER);
@@ -1076,7 +1060,7 @@ void vcpu_handle_exit(struct per_cpu *cpu_data)
 		iommu_check_pending_faults(cpu_data);
 		return;
 	case EXIT_REASON_CPUID:
-		vmx_handle_cpuid(&cpu_data->guest_regs);
+		vcpu_handle_cpuid();
 		return;
 	case EXIT_REASON_VMCALL:
 		vcpu_handle_hypercall();

commit ab06fa8a050134f84d1b66f4fff8d28594d809c7

x86: Fix cpuid.01h results regarding OSXSAVE
As we execute cpuid in the host on behalf of the guest, we have to
derive the OSXSAVE feature bit from the guest state of CR4 which
controls the availability of this while the guest is running.

Currently, if XSAVE is available and Jailhouse enables OSXSAVE for
itself in CR4, we always report it as set to the guest as well, causing
crashes or root cell applications that check for the availability via
cpuid and then run xgetbv.

Fixes: 10c6aac6eff4 ("x86: Always intercept cpuid")

Reported-by: Swaraj Dube <swaraj.dube@intel.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/include/asm/processor.h
@@ -25,6 +25,7 @@
 /* leaf 0x01, ECX */
 #define X86_FEATURE_VMX					(1 << 5)
 #define X86_FEATURE_XSAVE				(1 << 26)
+#define X86_FEATURE_OSXSAVE				(1 << 27)
 #define X86_FEATURE_HYPERVISOR				(1 << 31)
 
 /* leaf 0x07, subleaf 0, EBX */

hypervisor/arch/x86/vcpu.c
@@ -333,8 +333,14 @@ void vcpu_handle_cpuid(void)
 
 		cpuid((u32 *)&guest_regs->rax, (u32 *)&guest_regs->rbx,
 		      (u32 *)&guest_regs->rcx, (u32 *)&guest_regs->rdx);
-		if (function == 0x01 && this_cell() != &root_cell)
-			guest_regs->rcx |= X86_FEATURE_HYPERVISOR;
+		if (function == 0x01) {
+			if (this_cell() != &root_cell)
+				guest_regs->rcx |= X86_FEATURE_HYPERVISOR;
+
+			guest_regs->rcx &= ~X86_FEATURE_OSXSAVE;
+			if (vcpu_vendor_get_guest_cr4() & X86_CR4_OSXSAVE)
+				guest_regs->rcx |= X86_FEATURE_OSXSAVE;
+		}
 		break;
 	}
 