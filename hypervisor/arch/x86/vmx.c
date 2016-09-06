siemens/jailhouse

commit 427ff407c1ae4d8c2acfe657974f54b240c4af1b

x86: Check for availability of xsetbv before executing it for the guest
Practically, this feature is also available on VMX-capable CPUs, but we
should not rely on it.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/vmx.c
@@ -1077,7 +1077,8 @@ void vcpu_handle_exit(struct registers *guest_regs, struct per_cpu *cpu_data)
 		break;
 	case EXIT_REASON_XSETBV:
 		cpu_data->stats[JAILHOUSE_CPU_STAT_VMEXITS_XSETBV]++;
-		if (guest_regs->rax & X86_XCR0_FP &&
+		if (cpuid_ecx(1) & X86_FEATURE_XSAVE &&
+		    guest_regs->rax & X86_XCR0_FP &&
 		    (guest_regs->rax & ~cpuid_eax(0x0d)) == 0 &&
 		    guest_regs->rcx == 0 && guest_regs->rdx == 0) {
 			vcpu_skip_emulated_instruction(X86_INST_LEN_XSETBV);

hypervisor/arch/x86/include/asm/processor.h
@@ -19,6 +19,7 @@
 
 /* leaf 0x01, ECX */
 #define X86_FEATURE_VMX					(1 << 5)
+#define X86_FEATURE_XSAVE				(1 << 26)
 
 /* leaf 0x80000001, ECX */
 #define X86_FEATURE_SVM					(1 << 2)

commit 3174ef2f9198a828f80632c6b57c7efcc103647b

x86: Only execute xsetbv if actually enabled in the guest
Only perform xsetbv on behalf of the guest if it actually has it
enabled.

Fixes: 427ff407c1ae ("x86: Check for availability of xsetbv before
executing it for the guest")

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/vmx.c
@@ -1041,7 +1041,7 @@ static bool vmx_handle_xsetbv(void)
 {
 	union registers *guest_regs = &this_cpu_data()->guest_regs;
 
-	if (cpuid_ecx(1, 0) & X86_FEATURE_XSAVE &&
+	if (vcpu_vendor_get_guest_cr4() & X86_CR4_OSXSAVE &&
 	    guest_regs->rax & X86_XCR0_FP &&
 	    (guest_regs->rax & ~cpuid_eax(0x0d, 0)) == 0 &&
 	     guest_regs->rcx == 0 && guest_regs->rdx == 0) {