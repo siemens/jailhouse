84fa2d65f8dbb0918afceb4a8ceb0b69b343e051
Jan Kiszka <jan.kiszka@siemens.com>
2016-09-26 07:39:47 +0200

    configs: Add non-root Linux demo cells and device trees for Banana Pi
    Use the separate UART 7 for the inmate. Access to the CCU is granted to
    allow Linux to enable the related clock gate. We do not control the
    access on a bit-level, so this remains a hack. However, we can at least
    avoid that Linux touches the wrong bits by describing only the relevant
    one in the inmate device tree.
    
    On ARM, we have the need to align the virtual start address of the
    identity-mapped memory to 128 MB. Furthermore, Linux won't boot if that
    region starts at 0xf0000000 or higher.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ configs/bananapi-linux-demo.c
@@ -0,0 +1,82 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Configuration for linux-demo inmate on Banana Pi:
+ * 1 CPU, 64M RAM, serial port 7
+ *
+ * Copyright (c) Siemens AG, 2014-2016
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[4];
+	struct jailhouse_irqchip irqchips[1];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.name = "bananapi-linux-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
+	},
+
+	.cpus = {
+		0x2,
+	},
+
+	.mem_regions = {
+		/* CCU (HACK) */ {
+			.phys_start = 0x01c2006c,
+			.virt_start = 0x01c2006c,
+			.size = 0x4,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32,
+		},
+		/* UART 7 */ {
+			.phys_start = 0x01c29c00,
+			.virt_start = 0x01c29c00,
+			.size = 0x400,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32,
+		},
+		/* RAM */ {
+			.phys_start = 0x7bef0000,
+			.virt_start = 0,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* RAM */ {
+			.phys_start = 0x78000000,
+			.virt_start = 0x78000000,
+			.size = 0x3ef0000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_DMA |
+				JAILHOUSE_MEM_LOADABLE,
+		},
+	},
+
+	.irqchips = {
+		/* GIC */ {
+			.address = 0x01c81000,
+			.pin_base = 32,
+			.pin_bitmap = {
+				1 << (52-32)
+			},
+		},
+	},
+};
