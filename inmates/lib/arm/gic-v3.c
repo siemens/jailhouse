siemens/jailhouse

commit 9ee8e1418938960e756979290b4de7e1308efa42

arm: basic inmates demos
This patch adds the necessary libraries for writing simple inmates on arm,
and provides two demos.
It attempts to use the same layout as x86, and allows to set the devices
base addresses with platform-specific includes, in order to avoid
including kconfig.h

Only the vexpress platform with GICv2 and GICv3 is supported for the
moment.

Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
[Jan: Use config.mk instead of kbuild, use mmio accessors,
      avoid integer overflow, add copyright headers]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

inmates/lib/arm/gic-v3.c
@@ -1,0 +1,46 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+#include <asm/gic_common.h>
+#include <asm/gic_v3.h>
+#include <inmates/gic.h>
+#include <mach/gic_v3.h>
+
+void gic_enable(unsigned int irqn)
+{
+	if (is_spi(irqn))
+		mmio_write32(GICD_BASE + GICD_ISENABLER, 1 << irqn);
+	else
+		mmio_write32(GICR_BASE + GICR_SGI_BASE + GICR_ISENABLER,
+			     1 << irqn);
+}
+
+int gic_init(void)
+{
+	arm_write_sysreg(ICC_CTLR_EL1, 0);
+	arm_write_sysreg(ICC_PMR_EL1, 0xf0);
+	arm_write_sysreg(ICC_IGRPEN1_EL1, ICC_IGRPEN1_EN);
+
+	return 0;
+}
+
+void gic_write_eoi(u32 irqn)
+{
+	arm_write_sysreg(ICC_EOIR1_EL1, irqn);
+}
+
+u32 gic_read_ack(void)
+{
+	u32 val;
+
+	arm_read_sysreg(ICC_IAR1_EL1, val);
+	return val;
+}

inmates/lib/arm/include/inmates/gic.h
@@ -1,0 +1,25 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+#ifndef _JAILHOUSE_INMATES_GIC_H
+#define _JAILHOUSE_INMATES_GIC_H
+
+#include <jailhouse/types.h>
+
+#ifndef __ASSEMBLY__
+
+int gic_init(void);
+void gic_enable(unsigned int irqn);
+void gic_write_eoi(u32 irqn);
+u32 gic_read_ack(void);
+
+#endif /* !__ASSEMBLY__ */
+#endif

Kbuild
@@ -24,3 +24,8 @@ $(obj)/hypervisor/include/jailhouse/version.h: $(obj)/hypervisor
 	@
 
 $(obj)/driver.o: $(obj)/hypervisor/include/jailhouse/version.h
+
+# inmates build depends on generated config.mk of the hypervisor
+ifeq ($(clean),)
+$(addprefix $(obj)/,inmates): $(obj)/hypervisor
+endif

inmates/demos/arm/Makefile
@@ -1,0 +1,29 @@
+#
+# Jailhouse, a Linux-based partitioning hypervisor
+#
+# Copyright (c) ARM Limited, 2014
+#
+# Authors:
+#  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+#
+# This work is licensed under the terms of the GNU GPL, version 2.  See
+# the COPYING file in the top-level directory.
+#
+
+KBUILD_AFLAGS := $(filter-out -include asm/unified.h,$(KBUILD_AFLAGS))
+
+include $(INMATES_LIB)/Makefile.lib
+
+LIBDIR := ../../lib/arm
+
+INMATES := gic-demo.bin uart-demo.bin
+
+gic-demo-y := gic-demo.o \
+	$(addprefix $(LIBDIR)/,header.o printk.o gic.o $(DRIVERS))
+
+uart-demo-y := uart-demo.o \
+	$(addprefix $(LIBDIR)/,header.o printk.o $(DRIVERS))
+
+
+targets += $(INMATES:.bin=-y) $(INMATES:.bin=-linked.o) $(INMATES)
+always := $(INMATES)

inmates/demos/arm/gic-demo.c
@@ -1,0 +1,55 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <asm/gic_common.h>
+#include <asm/sysregs.h>
+#include <inmates/inmate.h>
+#include <mach/timer.h>
+
+static u64 tval;
+static u64 jiffies = 0;
+
+static void timer_arm(void)
+{
+	arm_write_sysreg(CNTV_TVAL_EL0, tval);
+	arm_write_sysreg(CNTV_CTL_EL0, 1);
+}
+
+static int timer_init(void)
+{
+	u32 freq = TIMER_FREQ;
+
+	tval = freq / 1000;
+	timer_arm();
+
+	return 0;
+}
+
+static void handle_IRQ(unsigned int irqn)
+{
+	if (irqn == TIMER_IRQ) {
+		printk("J=%d\n", (u32)jiffies++);
+		timer_arm();
+	}
+}
+
+void inmate_main(void)
+{
+	printk("Initializing the GIC...\n");
+	gic_setup(handle_IRQ);
+	gic_enable_irq(TIMER_IRQ);
+
+	printk("Initializing the timer...\n");
+	timer_init();
+
+	while (1);
+}

inmates/demos/arm/uart-demo.c
@@ -1,0 +1,41 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <inmates/inmate.h>
+
+/*
+ * To ease the debugging, we can send a spurious hypercall, which should return
+ * -ENOSYS, but appear in the hypervisor stats for this cell.
+ */
+static void heartbeat(void)
+{
+	asm volatile (
+	".arch_extension virt\n"
+	"mov	r0, %0\n"
+	"hvc	#0\n"
+	: : "r" (0xbea7) : "r0");
+}
+
+void inmate_main(void)
+{
+	unsigned int i = 0, j;
+	/*
+	 * The cell config can set up a mapping to access UARTx instead of UART0
+	 */
+	while(++i) {
+		for (j = 0; j < 100000000; j++);
+		printk("Hello %d from cell!\n", i);
+		heartbeat();
+	}
+
+	/* lr should be 0, so a return will go back to the reset vector */
+}

inmates/lib/arm/Makefile.lib
@@ -1,0 +1,37 @@
+#
+# Jailhouse, a Linux-based partitioning hypervisor
+#
+# Copyright (c) ARM Limited, 2014
+#
+# Authors:
+#  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+#
+# This work is licensed under the terms of the GNU GPL, version 2.  See
+# the COPYING file in the top-level directory.
+#
+
+-include $(obj)/../../../hypervisor/include/jailhouse/config.mk
+
+KBUILD_CFLAGS				+= -I$(INMATES_LIB)/include
+KBUILD_AFLAGE				+= -I$(INMATES_LIB)/include
+
+mach-$(CONFIG_ARCH_VEXPRESS)		:= vexpress
+
+gic-$(CONFIG_ARM_GIC)			:= gic-v2.o
+gic-$(CONFIG_ARM_GIC_V3)		:= gic-v3.o
+uart-$(CONFIG_SERIAL_AMBA_PL011) 	:= uart-pl011.o
+
+MACHINE					:= mach-$(mach-y)
+DRIVERS					:= $(gic-y) $(uart-y)
+KBUILD_CFLAGS				+= -I$(INMATES_LIB)/include/$(MACHINE)
+KBUILD_AFLAGS				+= -I$(INMATES_LIB)/include/$(MACHINE)
+
+# prevent deleting intermediate files which would cause rebuilds
+.SECONDARY: $(addprefix $(obj)/,$(targets))
+
+.SECONDEXPANSION:
+$(obj)/%-linked.o: $(INMATES_LIB)/inmate.lds $$(addprefix $$(obj)/,$$($$*-y))
+	$(call if_changed,ld)
+
+$(obj)/%.bin: $(obj)/%-linked.o
+	$(call if_changed,objcopy)

inmates/lib/arm/gic-v2.c
@@ -1,0 +1,39 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) ARM Limited, 2014
+ *
+ * Authors:
+ *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+#include <asm/gic_common.h>
+#include <asm/gic_v2.h>
+#include <inmates/gic.h>
+#include <inmates/inmate.h>
+#include <mach/gic_v2.h>
+
+void gic_enable(unsigned int irqn)
+{
+	mmio_write32(GICD_BASE + GICD_ISENABLER, 1 << irqn);
+}
+
+int gic_init(void)
+{
+	mmio_write32(GICC_BASE + GICC_CTLR, GICC_CTLR_GRPEN1);
+	mmio_write32(GICC_BASE + GICC_PMR, GICC_PMR_DEFAULT);
+
+	return 0;
+}
+
+void gic_write_eoi(u32 irqn)
+{
+	mmio_write32(GICC_BASE + GICC_EOIR, irqn);
+}
+
+u32 gic_read_ack(void)
+{
+	return mmio_read32(GICC_BASE + GICC_IAR);
+}

commit 02d8367908e05860e49d5860b71e36ba389f1cb8

inmates: arm: Fix GICv3 enabling
Found by Coverity: Make sure we only process SGIs, PPIs and SPIs.
Furthermore, we have to select the GICD_ISENABLER register for SPIs
based on the interrupt number first and then write the correct bit
between 0 and 31.

Fixes: 9ee8e1418938 ("arm: basic inmates demos")

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

inmates/lib/arm/gic-v3.c
@@ -27,11 +27,12 @@
 
 void gic_enable(unsigned int irqn)
 {
-	if (is_spi(irqn))
-		mmio_write32(GICD_BASE + GICD_ISENABLER, 1 << irqn);
-	else
+	if (is_sgi_ppi(irqn))
 		mmio_write32(GICR_BASE + GICR_SGI_BASE + GICR_ISENABLER,
 			     1 << irqn);
+	else if (is_spi(irqn))
+		mmio_write32(GICD_BASE + GICD_ISENABLER + irqn / 32,
+			     1 << (irqn % 32));
 }
 
 int gic_init(void)

inmates/lib/arm/include/gic.h
@@ -16,6 +16,7 @@
 
 #define GICD_ISENABLER			0x0100
 
+#define is_sgi_ppi(irqn)		((irqn) < 32)
 #define is_spi(irqn)			((irqn) > 31 && (irqn) < 1020)
 
 #ifndef __ASSEMBLY__