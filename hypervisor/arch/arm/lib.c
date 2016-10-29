a157335792cf55bbc898b9b4be49cd3c74a12b4c
Jan Kiszka <jan.kiszka@siemens.com>
2016-10-29 15:38:48 +0200

    arm: Provide __aeabi_llsl and __aeabi_llsr
    Will be required by the PCI layer. It's not worth optimizing this in
    assembly, the compiler is already pretty good in this.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ hypervisor/arch/arm/lib.c
@@ -0,0 +1,32 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2016
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <asm/types.h>
+
+unsigned long long __aeabi_llsl(unsigned long long val, unsigned int shift);
+unsigned long long __aeabi_llsr(unsigned long long val, unsigned int shift);
+
+unsigned long long __aeabi_llsl(unsigned long long val, unsigned int shift)
+{
+	u32 lo = (u32)val << shift;
+	u32 hi = ((u32)(val >> 32) << shift) | ((u32)val >> (32 - shift));
+
+	return ((unsigned long long)hi << 32) | lo;
+}
+
+unsigned long long __aeabi_llsr(unsigned long long val, unsigned int shift)
+{
+	u32 lo = ((u32)val >> shift) | ((u32)(val >> 32) << (32 - shift));
+	u32 hi = (u32)val >> shift;
+
+	return ((unsigned long long)hi << 32) | lo;
+}
