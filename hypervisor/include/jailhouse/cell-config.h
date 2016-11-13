siemens/jailhouse

commit 1bd483532cfd3a50d81bc4cd8697d79591c7ea83

core: Add support for sub-page MMIO regions
This allows to specify memory regions for MMIO accesses that do not
start or end on page boundaries. Instead of mapping full pages into the
cell, sub-page MMIO requires to intercept the page accesses, validate
all parameters against the target memory region and then perform the
access in hypervisor context, provided the validation was successful.

As the access can now fail in hypervisor context, we need to be more
picky: besides read/write permissions, alignment and access widths can
be checked additionally. These attributes are specified via the
JAILHOUSE_MEM_IO_* flags.

Sub-page MMIO is surely not a fast path. It not only requires world
switches between cell and hypervisor, the current implementation also
uses dynamic mappings. This is easier to implement than a static mapping
scheme, but surely not faster. We may revisit this design later on,
ideally towards a 1:1 mapping scheme.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/include/jailhouse/cell-config.h
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2014, 2015
+ * Copyright (c) Siemens AG, 2014-2016
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
@@ -67,6 +67,12 @@ struct jailhouse_cell_desc {
 #define JAILHOUSE_MEM_COMM_REGION	0x0020
 #define JAILHOUSE_MEM_LOADABLE		0x0040
 #define JAILHOUSE_MEM_ROOTSHARED	0x0080
+#define JAILHOUSE_MEM_IO_UNALIGNED	0x0100
+#define JAILHOUSE_MEM_IO_WIDTH_SHIFT	16 /* uses bits 8..11 */
+#define JAILHOUSE_MEM_IO_8		(1 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)
+#define JAILHOUSE_MEM_IO_16		(2 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)
+#define JAILHOUSE_MEM_IO_32		(4 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)
+#define JAILHOUSE_MEM_IO_64		(8 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)
 
 struct jailhouse_memory {
 	__u64 phys_start;
@@ -74,6 +80,9 @@ struct jailhouse_memory {
 	__u64 size;
 	__u64 flags;
 } __attribute__((packed));
+
+#define JAILHOUSE_MEMORY_IS_SUBPAGE(mem)	\
+	((mem)->virt_start & ~PAGE_MASK || (mem)->size & ~PAGE_MASK)
 
 #define JAILHOUSE_CACHE_L3_CODE		0x01
 #define JAILHOUSE_CACHE_L3_DATA		0x02

hypervisor/control.c
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013-2015
+ * Copyright (c) Siemens AG, 2013-2016
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
@@ -236,13 +236,19 @@ static bool address_in_region(unsigned long addr,
 static int unmap_from_root_cell(const struct jailhouse_memory *mem)
 {
 	/*
-	 * arch_unmap_memory_region uses the virtual address of the memory
-	 * region. As only the root cell has a guaranteed 1:1 mapping, make a
-	 * copy where we ensure this.
+	 * arch_unmap_memory_region and mmio_subpage_unregister use the
+	 * virtual address of the memory region for their job. As only the root
+	 * cell has a guaranteed 1:1 mapping, make a copy where we ensure this.
 	 */
 	struct jailhouse_memory tmp = *mem;
 
 	tmp.virt_start = tmp.phys_start;
+
+	if (JAILHOUSE_MEMORY_IS_SUBPAGE(&tmp)) {
+		mmio_subpage_unregister(&root_cell, &tmp);
+		return 0;
+	}
+
 	return arch_unmap_memory_region(&root_cell, &tmp);
 }
 
@@ -274,7 +280,10 @@ static int remap_to_root_cell(const struct jailhouse_memory *mem,
 			overlap.phys_start - root_mem->phys_start;
 		overlap.flags = root_mem->flags;
 
-		err = arch_map_memory_region(&root_cell, &overlap);
+		if (JAILHOUSE_MEMORY_IS_SUBPAGE(&overlap))
+			err = mmio_subpage_register(&root_cell, &overlap);
+		else
+			err = arch_map_memory_region(&root_cell, &overlap);
 		if (err) {
 			if (mode == ABORT_ON_ERROR)
 				break;
@@ -300,11 +309,14 @@ static void cell_destroy_internal(struct per_cpu *cpu_data, struct cell *cell)
 	}
 
 	for_each_mem_region(mem, cell->config, n) {
-		/*
-		 * This cannot fail. The region was mapped as a whole before,
-		 * thus no hugepages need to be broken up to unmap it.
-		 */
-		arch_unmap_memory_region(cell, mem);
+		if (!JAILHOUSE_MEMORY_IS_SUBPAGE(mem))
+			/*
+			 * This cannot fail. The region was mapped as a whole
+			 * before, thus no hugepages need to be broken up to
+			 * unmap it.
+			 */
+			arch_unmap_memory_region(cell, mem);
+
 		if (!(mem->flags & (JAILHOUSE_MEM_COMM_REGION |
 				    JAILHOUSE_MEM_ROOTSHARED)))
 			remap_to_root_cell(mem, WARN_ON_ERROR);
@@ -430,7 +442,10 @@ static int cell_create(struct per_cpu *cpu_data, unsigned long config_address)
 				goto err_destroy_cell;
 		}
 
-		err = arch_map_memory_region(cell, mem);
+		if (JAILHOUSE_MEMORY_IS_SUBPAGE(mem))
+			err = mmio_subpage_register(cell, mem);
+		else
+			err = arch_map_memory_region(cell, mem);
 		if (err)
 			goto err_destroy_cell;
 	}

hypervisor/include/jailhouse/mmio.h
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013-2015
+ * Copyright (c) Siemens AG, 2013-2016
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
@@ -15,6 +15,7 @@
 
 #include <jailhouse/types.h>
 #include <asm/mmio.h>
+#include <jailhouse/cell-config.h>
 
 struct cell;
 
@@ -170,6 +171,11 @@ enum mmio_result mmio_handle_access(struct mmio_access *mmio);
 void mmio_cell_exit(struct cell *cell);
 
 void mmio_perform_access(void *base, struct mmio_access *mmio);
+
+int mmio_subpage_register(struct cell *cell,
+			  const struct jailhouse_memory *mem);
+void mmio_subpage_unregister(struct cell *cell,
+			     const struct jailhouse_memory *mem);
 
 unsigned int arch_mmio_count_regions(struct cell *cell);
 

hypervisor/mmio.c
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2015
+ * Copyright (c) Siemens AG, 2015, 2016
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
@@ -11,6 +11,7 @@
  */
 
 #include <jailhouse/cell.h>
+#include <jailhouse/control.h>
 #include <jailhouse/mmio.h>
 #include <jailhouse/paging.h>
 #include <jailhouse/printk.h>
@@ -26,9 +27,15 @@
  */
 int mmio_cell_init(struct cell *cell)
 {
+	const struct jailhouse_memory *mem;
+	unsigned int n;
 	void *pages;
 
 	cell->max_mmio_regions = arch_mmio_count_regions(cell);
+
+	for_each_mem_region(mem, cell->config, n)
+		if (JAILHOUSE_MEMORY_IS_SUBPAGE(mem))
+			cell->max_mmio_regions++;
 
 	pages = page_alloc(&mem_pool,
 			   PAGES(cell->max_mmio_regions *
@@ -258,4 +265,68 @@ void mmio_perform_access(void *base, struct mmio_access *mmio)
 			break;
 #endif
 		}
+}
+
+static enum mmio_result mmio_handle_subpage(void *arg, struct mmio_access *mmio)
+{
+	const struct jailhouse_memory *mem = arg;
+	u64 perm = mmio->is_write ? JAILHOUSE_MEM_WRITE : JAILHOUSE_MEM_READ;
+	unsigned long page_virt = TEMPORARY_MAPPING_BASE +
+		this_cpu_id() * PAGE_SIZE * NUM_TEMPORARY_PAGES;
+	unsigned long page_phys =
+		((unsigned long)mem->phys_start + mmio->address) & PAGE_MASK;
+	unsigned long virt_base;
+	int err;
+
+	/* check read/write access permissions */
+	if (!(mem->flags & perm))
+		goto invalid_access;
+
+	/* width bit according to access size needs to be set */
+	if (!((mmio->size << JAILHOUSE_MEM_IO_WIDTH_SHIFT) & mem->flags))
+		goto invalid_access;
+
+	/* naturally unaligned access needs to be allowed explicitly */
+	if (mmio->address & (mmio->size - 1) &&
+	    !(mem->flags & JAILHOUSE_MEM_IO_UNALIGNED))
+		goto invalid_access;
+
+	err = paging_create(&hv_paging_structs, page_phys, PAGE_SIZE,
+			    page_virt, PAGE_DEFAULT_FLAGS | PAGE_FLAG_DEVICE,
+			    PAGING_NON_COHERENT);
+	if (err)
+		goto invalid_access;
+
+	/*
+	 * This virt_base gives the following effective virtual address in
+	 * mmio_perform_access:
+	 *
+	 *     page_virt + (mem->phys_start & ~PAGE_MASK) +
+	 *         (mmio->address & ~PAGE_MASK)
+	 *
+	 * Reason: mmio_perform_access does addr = base + mmio->address.
+	 */
+	virt_base = page_virt + (mem->phys_start & ~PAGE_MASK) -
+		(mmio->address & PAGE_MASK);
+	mmio_perform_access((void *)virt_base, mmio);
+	return MMIO_HANDLED;
+
+invalid_access:
+	panic_printk("FATAL: Invalid MMIO %s, address: %x, size: %x\n",
+		     mmio->is_write ? "write" : "read",
+		     mem->phys_start + mmio->address, mmio->size);
+	return MMIO_ERROR;
+}
+
+int mmio_subpage_register(struct cell *cell, const struct jailhouse_memory *mem)
+{
+	mmio_region_register(cell, mem->virt_start, mem->size,
+			     mmio_handle_subpage, (void *)mem);
+	return 0;
+}
+
+void mmio_subpage_unregister(struct cell *cell,
+			     const struct jailhouse_memory *mem)
+{
+	mmio_region_unregister(cell, mem->virt_start);
 }

hypervisor/setup.c
@@ -1,7 +1,7 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) Siemens AG, 2013, 2014
+ * Copyright (c) Siemens AG, 2013-2016
  *
  * Authors:
  *  Jan Kiszka <jan.kiszka@siemens.com>
@@ -13,6 +13,7 @@
 #include <jailhouse/processor.h>
 #include <jailhouse/printk.h>
 #include <jailhouse/entry.h>
+#include <jailhouse/mmio.h>
 #include <jailhouse/paging.h>
 #include <jailhouse/control.h>
 #include <jailhouse/string.h>
@@ -116,7 +117,10 @@ int map_root_memory_regions(void)
 	int err;
 
 	for_each_mem_region(mem, root_cell.config, n) {
-		err = arch_map_memory_region(&root_cell, mem);
+		if (JAILHOUSE_MEMORY_IS_SUBPAGE(mem))
+			err = mmio_subpage_register(&root_cell, mem);
+		else
+			err = arch_map_memory_region(&root_cell, mem);
 		if (err)
 			return err;
 	}

commit f47d6e5a854f143746f0341756fad7354b2315a7

core: fix comment
the comment does not reflect what the define actually states. 16..19 is
more reasonable.

Fixes: 1bd483532cfd ("core: Add support for sub-page MMIO regions")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/include/jailhouse/cell-config.h
@@ -75,7 +75,7 @@ struct jailhouse_cell_desc {
 #define JAILHOUSE_MEM_LOADABLE		0x0040
 #define JAILHOUSE_MEM_ROOTSHARED	0x0080
 #define JAILHOUSE_MEM_IO_UNALIGNED	0x0100
-#define JAILHOUSE_MEM_IO_WIDTH_SHIFT	16 /* uses bits 8..11 */
+#define JAILHOUSE_MEM_IO_WIDTH_SHIFT	16 /* uses bits 16..19 */
 #define JAILHOUSE_MEM_IO_8		(1 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)
 #define JAILHOUSE_MEM_IO_16		(2 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)
 #define JAILHOUSE_MEM_IO_32		(4 << JAILHOUSE_MEM_IO_WIDTH_SHIFT)