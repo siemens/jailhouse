siemens/jailhouse

commit 1b429b251090bf0a0cb23d7a3cd9a46efc2a924b

allow used of CC from build environnement
If you use a cross build system (yocto,...) you may want to use CC value
from build environnement.

Signed-off-by: Ronan <ronan.lemartret@iot.bzh>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

tools/Makefile
@@ -10,7 +10,7 @@
 # the COPYING file in the top-level directory.
 #
 
-CC = $(CROSS_COMPILE)gcc
+CC ?= $(CROSS_COMPILE)gcc
 
 CFLAGS = -g -O3 -I../driver -DLIBEXECDIR=\"$(libexecdir)\" \
 	-Wall -Wextra -Wmissing-declarations -Wmissing-prototypes -Werror \

commit 4abab3e6700d643c2bcd35c7e4eafd9cfb5ee072

tooling: Build tools directory via kbuild
This solves the regression of 1b429b251090 - broken tools cross-build
if not overwriting CC - by using kbuild also for building the tools.
Installation still works by calling explicitly into that directory.

Fixes: 1b429b251090 ("allow used of CC from build environnement")

CC: Ronan Lemartret <ronan.lemartret@iot.bzh>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

Kbuild
@@ -11,7 +11,7 @@
 # the COPYING file in the top-level directory.
 #
 
-subdir-y := driver hypervisor configs inmates
+subdir-y := driver hypervisor configs inmates tools
 
 subdir-ccflags-y := -Werror
 

Makefile
@@ -17,7 +17,7 @@ $(error Too old make version $(MAKE_VERSION), at least $(need) required)
 endif
 
 # no recipes above this one (also no includes)
-all: modules tools
+all: modules
 
 # includes installation-related variables and definitions
 include scripts/include.mk
@@ -35,10 +35,6 @@ kbuild = -C $(KDIR) M=$$PWD $@
 modules:
 	$(Q)$(MAKE) $(kbuild)
 
-# recursive build of tools
-tools:
-	$(Q)$(MAKE) -C tools
-
 # documentation, build needs to be triggered explicitly
 docs:
 	$(DOXYGEN) Documentation/Doxyfile
@@ -48,9 +44,8 @@ docs_clean:
 	rm -rf Documentation/generated
 
 # clean up kernel, tools and generated docs
-clean:	docs_clean
+clean: docs_clean
 	$(Q)$(MAKE) $(kbuild)
-	$(Q)$(MAKE) -C tools $@
 
 modules_install: modules
 	$(Q)$(MAKE) $(kbuild)
@@ -65,7 +60,7 @@ tool_inmates_install: $(DESTDIR)$(libexecdir)/jailhouse
 endif
 
 install: modules_install firmware_install $(TOOL_INMATES_INSTALL)
-	$(Q)$(MAKE) -C tools $@
+	$(Q)$(MAKE) -C tools $@ src=.
 
 .PHONY: modules_install install clean firmware_install modules tools docs \
 	docs_clean

tools/Makefile
@@ -1,7 +1,7 @@
 #
 # Jailhouse, a Linux-based partitioning hypervisor
 #
-# Copyright (c) Siemens AG, 2013, 2014
+# Copyright (c) Siemens AG, 2013-2016
 #
 # Authors:
 #  Jan Kiszka <jan.kiszka@siemens.com>
@@ -10,15 +10,18 @@
 # the COPYING file in the top-level directory.
 #
 
-CC ?= $(CROSS_COMPILE)gcc
+# includes installation-related variables and definitions
+include $(src)/../scripts/include.mk
 
-CFLAGS = -g -O3 -I../driver -DLIBEXECDIR=\"$(libexecdir)\" \
+LD = $(CC)
+NOSTDINC_FLAGS :=
+LINUXINCLUDE := -I$(src)/../driver
+KBUILD_CFLAGS := -g -O3 -DLIBEXECDIR=\"$(libexecdir)\" \
 	-Wall -Wextra -Wmissing-declarations -Wmissing-prototypes -Werror \
-	-DJAILHOUSE_VERSION=\"$(shell cat ../VERSION)\" $(EXTRA_CFLAGS)
+	-DJAILHOUSE_VERSION=\"$(shell cat $(src)/../VERSION)\" $(EXTRA_CFLAGS)
+LDFLAGS :=
 
-TARGETS := jailhouse
-
-INST_TARGETS := $(TARGETS)
+BINARIES := jailhouse
 HELPERS := \
 	jailhouse-cell-linux \
 	jailhouse-cell-stats \
@@ -26,35 +29,38 @@ HELPERS := \
 	jailhouse-hardware-check
 TEMPLATES := jailhouse-config-collect.tmpl root-cell-config.c.tmpl
 
+always := $(BINARIES)
+
 HAS_PYTHON_MAKO := \
 	$(shell python -c "from mako.template import Template" 2>/dev/null \
 	&& echo yes)
 
 ifeq ($(strip $(HAS_PYTHON_MAKO)), yes)
-	TARGETS += jailhouse-config-collect
+	always += jailhouse-config-collect
 	HELPERS += jailhouse-config-collect
 else
-	TARGETS += no_python_mako
+	always += no_python_mako
 endif
 
 define patch_dirvar
 	sed -i 's|^$1 = None|$1 = "$($1)"|' $2
 endef
 
-# no recipes above this one (also no includes)
-all: $(TARGETS)
+quiet_cmd_gen_collect = GEN     $@
+define cmd_gen_collect
+	$< -g $@; \
+	chmod +x $@
+endef
 
-# includes installation-related variables and definitions
-include ../scripts/include.mk
+targets += jailhouse.o
 
-jailhouse: jailhouse.c ../driver/jailhouse.h ../VERSION
-	$(CC) $(CFLAGS) -o $@ $<
+$(obj)/jailhouse: $(obj)/jailhouse.o
+	$(call if_changed,ld)
 
-jailhouse-config-collect: jailhouse-config-create jailhouse-config-collect.tmpl
-	./$< -g $@
-	$(Q)chmod +x $@
+$(obj)/jailhouse-config-collect: $(src)/jailhouse-config-create $(src)/jailhouse-config-collect.tmpl
+	$(call if_changed,gen_collect)
 
-install-bin: $(INST_TARGETS) $(DESTDIR)$(sbindir)
+install-bin: $(BINARIES) $(DESTDIR)$(sbindir)
 	$(INSTALL_PROGRAM) $^
 
 install-libexec: $(HELPERS) $(DESTDIR)$(libexecdir)/jailhouse
@@ -67,13 +73,10 @@ install-data: $(TEMPLATES) $(DESTDIR)$(datadir)/jailhouse
 
 install: install-bin install-libexec install-data
 
-clean:
-	rm -f $(TARGETS)
-
-no_python_mako:
+$(obj)/no_python_mako:
 	@echo -ne "WARNING: Could not create the helper script to generate" \
 		  "configurations on remote machines" \
 		  "(\"jailhouse-conf-collect\"). You need Python and the" \
 		  "Mako library for it.\n"
 
-.PHONY: install install-bin install-libexec install-data clean no_python_mako
+.PHONY: install install-bin install-libexec install-data no_python_mako