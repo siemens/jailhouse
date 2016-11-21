4223f8c78be39cb649d565f78640ddbb0665293b
Jan Kiszka <jan.kiszka@siemens.com>
2016-11-21 08:45:11 +0100

    configs: Add non-root Linux demo cells and device trees for HiKey
    For now, we share the UART 3 with the root cell. Can be changed to a
    separate port as needed.
    
    To test SMP support, to CPUs are handed out to the non-root cell.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ configs/hikey-linux-demo.c
@@ -0,0 +1,75 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Configuration for linux-demo inmate on HiKey:
+ * 1 CPU, 128M RAM, serial port 3
+ *
+ * Copyright (c) Siemens AG, 2014-2016
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[3];
+	struct jailhouse_irqchip irqchips[1];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.name = "hikey-linux-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
+	},
+
+	.cpus = {
+		0xc0,
+	},
+
+	.mem_regions = {
+		/* UART 3 */ {
+			.phys_start = 0xf7113000,
+			.virt_start = 0xf7113000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_ROOTSHARED,
+		},
+		/* RAM */ {
+			.phys_start = 0x7bef0000,
+			.virt_start = 0,
+			.size = 0x10000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+		/* RAM */ {
+			.phys_start = 0x74000000,
+			.virt_start = 0x74000000,
+			.size = 0x7ef0000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_DMA |
+				JAILHOUSE_MEM_LOADABLE,
+		},
+	},
+
+	.irqchips = {
+		/* GIC */ {
+			.address = 0xf6801000,
+			.pin_base = 32,
+			.pin_bitmap = {
+				0, 1 << (71 - 64)
+			},
+		},
+	},
+};
