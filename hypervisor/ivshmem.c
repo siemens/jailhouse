f85c225feaeadc0a4c19955dc8cfbaa4414d092e
Henning Schild <henning.schild@siemens.com>
2016-11-30 17:50:22 +0100

    core: ivshmem: match device class to connect
    Since 3e970ab533c4 we can specify a protocol to run on the ivshmem channel.
    This patch considers a protocol mismatch between the two endpoints a
    configuration error and does not connect them to each other.
    
    Signed-off-by: Henning Schild <henning.schild@siemens.com>
    [Jan: rebased]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/ivshmem.c
+++ hypervisor/ivshmem.c
@@ -337,9 +337,10 @@ int ivshmem_init(struct cell *cell, struct pci_device *device)
 		peer_mem = jailhouse_cell_mem_regions(peer_dev->cell->config) +
 			peer_dev->info->shmem_region;
 
-		/* check that the regions of both peers match */
+		/* check that the regions and protocols of both peers match */
 		if (peer_mem->phys_start != mem->phys_start ||
-		    peer_mem->size != mem->size)
+		    peer_mem->size != mem->size ||
+		    peer_dev->info->shmem_protocol != dev_info->shmem_protocol)
 			return trace_error(-EINVAL);
 
 		printk("Shared memory connection established: "
