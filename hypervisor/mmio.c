siemens/jailhouse

commit 0993685e882f5767290d9712e898e019003de6b9

core: Add generic MMIO access dispatching
This introduces the infrastructure for generically dispatching MMIO
accesses. Handlers can now be registered for MMIO regions on a pre-cell
basis, removing the need for probing multiple handlers that decide
themselves if they are in charge.

The backing data structures consist of two sorted tables: one is
describing the region locations and sizes, the other one is holding, in
identical order, the handler and a corresponding opaque parameter.

Dispatching works lock-free and can even run in parallel with region
registration or removal. That latter two steps are protected against
concurrent invocation via a per-cell spinlock.

In order to preallocate sufficient space during cell setup, arch
architecture has to implement arch_mmio_count_regions that calculates
the maximum number of MMIO regions a cell may register during its
lifetime, typically based on static values and the cell configuration.
So far these functions are implemented as dummies because the MMIO
infrastructure is not yet used.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/mmio.c
@@ -1,0 +1,221 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2015
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/cell.h>
+#include <jailhouse/mmio.h>
+#include <jailhouse/paging.h>
+#include <jailhouse/printk.h>
+#include <asm/percpu.h>
+
+/**
+ * Perform MMIO-specific initialization for a new cell.
+ * @param cell		Cell to be initialized.
+ *
+ * @return 0 on success, negative error code otherwise.
+ *
+ * @see mmio_cell_exit
+ */
+int mmio_cell_init(struct cell *cell)
+{
+	void *pages;
+
+	cell->max_mmio_regions = arch_mmio_count_regions(cell);
+
+	pages = page_alloc(&mem_pool,
+			   PAGES(cell->max_mmio_regions *
+				 (sizeof(struct mmio_region_location) +
+				  sizeof(struct mmio_region_handler))));
+	if (!pages)
+		return -ENOMEM;
+
+	cell->mmio_locations = pages;
+	cell->mmio_handlers = pages +
+		cell->max_mmio_regions * sizeof(struct mmio_region_location);
+
+	return 0;
+}
+
+static void copy_region(struct cell *cell, unsigned int src, unsigned dst)
+{
+	/*
+	 * Invalidate destination region by shrinking it to size 0. This has to
+	 * be made visible to other CPUs via a memory barrier before
+	 * manipulating other destination fields.
+	 */
+	cell->mmio_locations[dst].size = 0;
+	memory_barrier();
+
+	cell->mmio_locations[dst].start = cell->mmio_locations[src].start;
+	cell->mmio_handlers[dst] = cell->mmio_handlers[src];
+	/* Ensure all fields are committed before activating the region. */
+	memory_barrier();
+
+	cell->mmio_locations[dst].size = cell->mmio_locations[src].size;
+}
+
+/**
+ * Register a MMIO region access handler for a cell.
+ * @param cell		Cell than can access the region.
+ * @param start		Region start address in cell address space.
+ * @param size		Region size.
+ * @param handler	Access handler.
+ * @param handler_arg	Opaque argument to pass to handler.
+ *
+ * @see mmio_region_unregister
+ */
+void mmio_region_register(struct cell *cell, unsigned long start,
+			  unsigned long size, mmio_handler handler,
+			  void *handler_arg)
+{
+	unsigned int index, n;
+
+	spin_lock(&cell->mmio_region_lock);
+
+	if (cell->num_mmio_regions >= cell->max_mmio_regions) {
+		spin_unlock(&cell->mmio_region_lock);
+
+		printk("WARNING: Overflow during MMIO region registration!\n");
+		return;
+	}
+
+	for (index = 0; index < cell->num_mmio_regions; index++)
+		if (cell->mmio_locations[index].start > start)
+			break;
+
+	/*
+	 * Set and commit a dummy region at the end if the list so that
+	 * we can safely grow it.
+	 */
+	cell->mmio_locations[cell->num_mmio_regions].start = -1;
+	cell->mmio_locations[cell->num_mmio_regions].size = 0;
+	memory_barrier();
+
+	/*
+	 * Extend region list by one so that we can start moving entries.
+	 * Commit this change via a barrier so that the current last element
+	 * will remain visible when moving it up.
+	 */
+	cell->num_mmio_regions++;
+	memory_barrier();
+
+	for (n = cell->num_mmio_regions - 1; n > index; n--)
+		copy_region(cell, n - 1, n);
+
+	/* Invalidate the new region entry first (see also copy_region()). */
+	cell->mmio_locations[index].size = 0;
+	memory_barrier();
+
+	cell->mmio_locations[index].start = start;
+	cell->mmio_handlers[index].handler = handler;
+	cell->mmio_handlers[index].arg = handler_arg;
+	/* Ensure all fields are committed before activating the region. */
+	memory_barrier();
+
+	cell->mmio_locations[index].size = size;
+
+	spin_unlock(&cell->mmio_region_lock);
+}
+
+static int find_region(struct cell *cell, unsigned long address,
+		       unsigned int size)
+{
+	unsigned int range_start = 0;
+	unsigned int range_size = cell->num_mmio_regions;
+	struct mmio_region_location region;
+	unsigned int index;
+
+	while (range_size > 0) {
+		index = range_start + range_size / 2;
+		region = cell->mmio_locations[index];
+
+		if (address < region.start) {
+			range_size = index - range_start;
+		} else if (region.start + region.size < address + size) {
+			range_size -= index + 1 - range_start;
+			range_start = index + 1;
+		} else {
+			return index;
+		}
+	}
+	return -1;
+}
+
+/**
+ * Unregister MMIO region from a cell.
+ * @param cell		Cell the region belongs to.
+ * @param start		Region start address as it was passed to
+ * 			mmio_region_register().
+ *
+ * @see mmio_region_register
+ */
+void mmio_region_unregister(struct cell *cell, unsigned long start)
+{
+	int index;
+
+	spin_lock(&cell->mmio_region_lock);
+
+	index = find_region(cell, start, 0);
+	if (index >= 0) {
+		for (/* empty */; index < cell->num_mmio_regions; index++)
+			copy_region(cell, index + 1, index);
+
+		/*
+		 * Ensure the last region move is visible before shrinking the
+		 * list.
+		 */
+		memory_barrier();
+
+		cell->num_mmio_regions--;
+	}
+	spin_unlock(&cell->mmio_region_lock);
+}
+
+/**
+ * Dispatch MMIO access of a cell CPU.
+ * @param mmio		MMIO access description. @a mmio->value will receive the
+ * 			result of a successful read access. All @a mmio fields
+ * 			may have been modified on return.
+ *
+ * @return MMIO_HANDLED on success, MMIO_UNHANDLED if no region is registered
+ * for the access address and size, or MMIO_ERROR if an access error was
+ * detected.
+ *
+ * @see mmio_region_register
+ * @see mmio_region_unregister
+ */
+enum mmio_result mmio_handle_access(struct mmio_access *mmio)
+{
+	struct cell *cell = this_cell();
+	int index = find_region(cell, mmio->address, mmio->size);
+	mmio_handler handler;
+
+	if (index < 0)
+		return MMIO_UNHANDLED;
+
+	handler = cell->mmio_handlers[index].handler;
+	mmio->address -= cell->mmio_locations[index].start;
+	return handler(cell->mmio_handlers[index].arg, mmio);
+}
+
+/**
+ * Perform MMIO-specific cleanup for a cell under destruction.
+ * @param cell		Cell to be destructed.
+ *
+ * @see mmio_cell_init
+ */
+void mmio_cell_exit(struct cell *cell)
+{
+	page_free(&mem_pool, cell->mmio_locations,
+		  PAGES(cell->max_mmio_regions *
+			(sizeof(struct mmio_region_location) +
+			 sizeof(struct mmio_region_handler))));
+}

hypervisor/arch/arm/mmio.c
@@ -16,6 +16,11 @@
 #include <asm/smp.h>
 #include <asm/traps.h>
 
+unsigned int arch_mmio_count_regions(struct cell *cell)
+{
+	return 1;
+}
+
 /* Taken from the ARM ARM pseudocode for taking a data abort */
 static void arch_inject_dabt(struct trap_context *ctx, unsigned long addr)
 {

hypervisor/arch/x86/mmio.c
@@ -170,3 +170,8 @@ error:
 	inst.inst_len = 0;
 	return inst;
 }
+
+unsigned int arch_mmio_count_regions(struct cell *cell)
+{
+	return 1;
+}

hypervisor/Makefile
@@ -37,7 +37,7 @@ ifneq ($(wildcard $(obj)/include/jailhouse/config.h),)
 KBUILD_CFLAGS += -include $(obj)/include/jailhouse/config.h
 endif
 
-CORE_OBJECTS = setup.o printk.o paging.o control.o lib.o
+CORE_OBJECTS = setup.o printk.o paging.o control.o lib.o mmio.o
 
 define filechk_config_mk
 (									\

hypervisor/control.c
@@ -12,6 +12,7 @@
 
 #include <jailhouse/entry.h>
 #include <jailhouse/control.h>
+#include <jailhouse/mmio.h>
 #include <jailhouse/printk.h>
 #include <jailhouse/paging.h>
 #include <jailhouse/processor.h>
@@ -175,6 +176,7 @@ int cell_init(struct cell *cell)
 		jailhouse_cell_cpu_set(cell->config);
 	unsigned long cpu_set_size = cell->config->cpu_set_size;
 	struct cpu_set *cpu_set;
+	int err;
 
 	cell->id = get_free_cell_id();
 
@@ -192,11 +194,17 @@ int cell_init(struct cell *cell)
 
 	cell->cpu_set = cpu_set;
 
-	return 0;
+	err = mmio_cell_init(cell);
+	if (err && cell->cpu_set != &cell->small_cpu_set)
+		page_free(&mem_pool, cell->cpu_set, 1);
+
+	return err;
 }
 
 static void cell_exit(struct cell *cell)
 {
+	mmio_cell_exit(cell);
+
 	if (cell->cpu_set != &cell->small_cpu_set)
 		page_free(&mem_pool, cell->cpu_set, 1);
 }

hypervisor/include/jailhouse/cell.h
@@ -13,9 +13,11 @@
 #ifndef _JAILHOUSE_CELL_H
 #define _JAILHOUSE_CELL_H
 
+#include <jailhouse/mmio.h>
 #include <jailhouse/paging.h>
 #include <jailhouse/pci.h>
 #include <asm/cell.h>
+#include <asm/spinlock.h>
 
 #include <jailhouse/cell-config.h>
 #include <jailhouse/hypercall.h>
@@ -58,6 +60,18 @@ struct cell {
 	struct pci_device *msix_device_list;
 	/** List of virtual PCI devices assigned to this cell. */
 	struct pci_device *virtual_device_list;
+
+	/** Lock protecting changes to mmio_locations, mmio_handlers, and
+	 * num_mmio_regions. */
+	spinlock_t mmio_region_lock;
+	/** MMIO region description table. */
+	struct mmio_region_location *mmio_locations;
+	/** MMIO region handler table. */
+	struct mmio_region_handler *mmio_handlers;
+	/** Number of MMIO regions in use. */
+	unsigned int num_mmio_regions;
+	/** Maximum number of MMIO regions. */
+	unsigned int max_mmio_regions;
 };
 
 extern struct cell root_cell;

hypervisor/include/jailhouse/mmio.h
@@ -16,6 +16,8 @@
 #include <jailhouse/types.h>
 #include <asm/mmio.h>
 
+struct cell;
+
 /**
  * @defgroup IO I/O Access Subsystem
  *
@@ -115,6 +117,9 @@ static inline void mmio_write64_field(void *address, u64 mask, u64 value)
 }
 /** @} */
 
+/** MMIO access result. */
+enum mmio_result {MMIO_ERROR = -1, MMIO_UNHANDLED, MMIO_HANDLED};
+
 /** MMIO access description. */
 struct mmio_access {
 	/** Address to access, depending on the context, an absolute address or
@@ -127,6 +132,44 @@ struct mmio_access {
 	/** The value to be written or the read value to return. */
 	unsigned long value;
 };
+
+/** MMIO callback handler.
+ * @param arg		Opaque argument defined via mmio_region_register().
+ * @param mmio		MMIO access description. @a mmio->address will be
+ * 			provided as offset to the region start.
+ *
+ * @return MMIO_HANDLED on success, MMIO_ERROR otherwise.
+ */
+typedef enum mmio_result (*mmio_handler)(void *arg, struct mmio_access *mmio);
+
+/** MMIO region coordinates. */
+struct mmio_region_location {
+	/** Start address of the region. */
+	unsigned long start;
+	/** Region size. */
+	unsigned long size;
+};
+
+/** MMIO region access callback description. */
+struct mmio_region_handler {
+	/** Access handler. */
+	mmio_handler handler;
+	/** Argument to pass to the handler. */
+	void *arg;
+};
+
+int mmio_cell_init(struct cell *cell);
+
+void mmio_region_register(struct cell *cell, unsigned long start,
+			  unsigned long size, mmio_handler handler,
+			  void *handler_arg);
+void mmio_region_unregister(struct cell *cell, unsigned long start);
+
+enum mmio_result mmio_handle_access(struct mmio_access *mmio);
+
+void mmio_cell_exit(struct cell *cell);
+
+unsigned int arch_mmio_count_regions(struct cell *cell);
 
 /** @} */
 #endif /* !_JAILHOUSE_MMIO_H */

commit d10687fc401fbae565bdfb704e5719345efa7692

core: Fix matching on mmio_region_unregister
Looking up the to-be-deleted region with zero legnth can cause false
matches:

Registered regions:
      Start       Size
 0xb0000000 0x10000000
0x100000000     0x1000
0x100002000       0x20
0x100002020       0x20

Now trying to remove 0x100002020 will match on 0x100002000. This can be
trivially avoided by looking for a 1-byte region.

Fixes: 0993685e882f ("core: Add generic MMIO access dispatching")

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/mmio.c
@@ -171,7 +171,7 @@ void mmio_region_unregister(struct cell *cell, unsigned long start)
 
 	spin_lock(&cell->mmio_region_lock);
 
-	index = find_region(cell, start, 0);
+	index = find_region(cell, start, 1);
 	if (index >= 0) {
 		for (/* empty */; index < cell->num_mmio_regions; index++)
 			copy_region(cell, index + 1, index);