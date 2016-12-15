siemens/jailhouse

commit 4fd3748467c465acbe5384805cefde32667e7389

configs: Add XUSB to jetson-tk1 config
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/jetson-tk1.c
@@ -23,7 +23,7 @@
 struct {
 	struct jailhouse_system header;
 	__u64 cpus[1];
-	struct jailhouse_memory mem_regions[11];
+	struct jailhouse_memory mem_regions[12];
 	struct jailhouse_irqchip irqchips[3];
 } __attribute__((packed)) config = {
 	.header = {
@@ -90,6 +90,13 @@ struct {
 			.phys_start = 0x70019000,
 			.virt_start = 0x70019000,
 			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* XUSB */ {
+			.phys_start = 0x70090000,
+			.virt_start = 0x70090000,
+			.size = 0x8000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},

commit cfe6abfef4580c15a77f03b56e86229bc09c23a0

configs: fix size of TK1's XUSB memory region
This should be 0xa000 instead of 0x8000. According to Linux's
arch/arm/boot/dts/tegra124.dtsi, XUSB claims:

reg = <0x0 0x70090000 0x0 0x8000>,
      <0x0 0x70098000 0x0 0x1000>,
      <0x0 0x70099000 0x0 0x1000>;

Fixes: 4fd3748467c ("configs: Add XUSB to jetson-tk1 config")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/jetson-tk1.c
@@ -166,7 +166,7 @@ struct {
 		/* XUSB */ {
 			.phys_start = 0x70090000,
 			.virt_start = 0x70090000,
-			.size = 0x8000,
+			.size = 0xa000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},