9747418070bda31dc11ba0e889674ca99a7c0913
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2017-01-25 13:11:46 +0100

    tools: dump error console on failures
    From now on, 'jailhouse enable' will dump the console if an error occurs
    during initialisation of the hypervisor.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    Reviewed-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- tools/jailhouse.c
+++ tools/jailhouse.c
@@ -228,8 +228,10 @@ static int enable(int argc, char *argv[])
 	fd = open_dev();
 
 	err = ioctl(fd, JAILHOUSE_ENABLE, config);
-	if (err)
+	if (err) {
 		perror("JAILHOUSE_ENABLE");
+		dump_console(fd, true);
+	}
 
 	close(fd);
 	free(config);
