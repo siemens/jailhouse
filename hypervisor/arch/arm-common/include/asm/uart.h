c7c988071497bbdc90960bb4b68e0c3bd4cdb1f8
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2017-03-01 21:56:25 -0700

    core: arm: uart: remove include guards
    We only have a single includer, hypervisor/arch/arm-common/dbg-write.c
    
    Remove Jean-Philippe from author list -- there is no line of code
    that came from him left in this file.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [Jan: also remove unneeded __ASSEMBLY__ guards]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/arch/arm-common/include/asm/uart.h
+++ hypervisor/arch/arm-common/include/asm/uart.h
@@ -1,21 +1,13 @@
 /*
  * Jailhouse, a Linux-based partitioning hypervisor
  *
- * Copyright (c) ARM Limited, 2014
+ * Copyright (c) OTH Regensburg, 2017
  *
  * Authors:
- *  Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
 
-#ifndef JAILHOUSE_ASM_UART_H_
-#define JAILHOUSE_ASM_UART_H_
-
-#ifndef __ASSEMBLY__
-
 extern struct uart_chip uart_pl011_ops, uart_xuartps_ops;
-
-#endif /* !__ASSEMBLY__ */
-#endif /* !JAILHOUSE_ASM_UART_H_ */
