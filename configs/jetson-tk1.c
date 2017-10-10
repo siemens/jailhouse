siemens/jailhouse

commit 592ebc72960bbcaf7bf3b48b927a0d3bdf9898ac

configs: TK1: reorder memory regions
This commit reorders memory regions by ascending physical addresses,
which improves the readability of the config file.

Additionally, let's spend an additional comment on the apbmisc region,
as it is actually split into two regions:
  0x70000800, size 0x64 (Chip Revision)
  0x7000e864, size 0x04 (Strapping Options)

(cf. Linux's tegra124.dtsi)

No functional change.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/jetson-tk1.c
@@ -135,10 +135,17 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},
-		/* HACK: CPU_DFLL clock */ {
-			.phys_start = 0x70110000,
-			.virt_start = 0x70110000,
-			.size = 0x00001000,
+		/* HACK: apbmisc "Chip Revision" */ {
+			.phys_start = 0x70000800,
+			.virt_start = 0x70000800,
+			.size = 0x00000100,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+					JAILHOUSE_MEM_IO,
+		},
+		/* UART */ {
+			.phys_start = 0x70006000,
+			.virt_start = 0x70006000,
+			.size = 0x1000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},
@@ -152,6 +159,13 @@ struct {
 		/* I2C5/6, SPI */ {
 			.phys_start = 0x7000d000,
 			.virt_start = 0x7000d000,
+			.size = 0x00001000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* RTC + PMC + apbmisc "Strapping Options" */ {
+			.phys_start = 0x7000e000,
+			.virt_start = 0x7000e000,
 			.size = 0x00001000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
@@ -177,31 +191,17 @@ struct {
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},
-		/* RTC + PMC */ {
-			.phys_start = 0x7000e000,
-			.virt_start = 0x7000e000,
+		/* HACK: CPU_DFLL clock */ {
+			.phys_start = 0x70110000,
+			.virt_start = 0x70110000,
 			.size = 0x00001000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
-		},
-		/* HACK: apbmisc */ {
-			.phys_start = 0x70000800,
-			.virt_start = 0x70000800,
-			.size = 0x00000100,
-			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
-					JAILHOUSE_MEM_IO,
 		},
 		/* USB */ {
 			.phys_start = 0x7d004000,
 			.virt_start = 0x7d004000,
 			.size = 0x00008000,
-			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
-				JAILHOUSE_MEM_IO,
-		},
-		/* UART */ {
-			.phys_start = 0x70006000,
-			.virt_start = 0x70006000,
-			.size = 0x1000,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
 				JAILHOUSE_MEM_IO,
 		},

commit 2be7ea62732df5899ab3002aee89004db06eba2a

configs: TK1: allow 32-bit wide access to apbmisc
The following crash is observed since 4.14-rc4:

FATAL: Invalid MMIO read, address: 70000804, size: 4
FATAL: forbidden access (exception class 0x24)
pc=0xc0499424 cpsr=0x20010093 hsr=0x93800007
r0=0x0000002f r1=0xa0010093 r2=0x00000004 r3=0xfe000800
r4=0xc0e21650 r5=0xde40fc80 r6=0xde5c7488 r7=0x0000000a
r8=0xdddfc000 r9=0x00000008 r10=0xc04e6998 r11=0xdddf6d80
r12=0x00000000 r13=0xdddfde80 r14=0xc049060c
Parking CPU 0 (Cell: "Jetson-TK1")

PC points to:
c0499418 <tegra_read_chipid>:
c0499418:       e30d34b8        movw    r3, #54456      ; 0xd4b8
c049941c:       e34c30e9        movt    r3, #49385      ; 0xc0e9
c0499420:       e5933000        ldr     r3, [r3]
c0499424:       e5930004        ldr     r0, [r3, #4]    ; <-- Peng
c0499428:       e12fff1e        bx      lr

While this area is actually exposed to the root cell as subpage, the
hypervisor panics the cell because no width is specified. Accesses are
apparently 32-bit wide, so let's allow 32-bit accesses.

Fixes: 592ebc72960bb ("configs: TK1: reorder memory regions")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/jetson-tk1.c
@@ -141,7 +141,7 @@ struct {
 			.virt_start = 0x70000800,
 			.size = 0x00000100,
 			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
-					JAILHOUSE_MEM_IO,
+				JAILHOUSE_MEM_IO | JAILHOUSE_MEM_IO_32,
 		},
 		/* UART */ {
 			.phys_start = 0x70006000,