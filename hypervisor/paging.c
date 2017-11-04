c535fb580bd3c06056baa0ee416328c809f34521
Jan Kiszka <jan.kiszka@siemens.com>
2017-11-04 20:41:13 +0100

    core: Reindent paging_gvirt2gphys
    No functional changes.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/paging.c
+++ hypervisor/paging.c
@@ -423,9 +423,8 @@ paging_gvirt2gphys(const struct guest_paging_structures *pg_structs,
 
 	while (1) {
 		/* map guest page table */
-		phys = arch_paging_gphys2phys(this_cpu_data(),
-						page_table_gphys,
-						PAGE_READONLY_FLAGS);
+		phys = arch_paging_gphys2phys(this_cpu_data(), page_table_gphys,
+					      PAGE_READONLY_FLAGS);
 		if (phys == INVALID_PHYS_ADDR)
 			return INVALID_PHYS_ADDR;
 		err = paging_create(&hv_paging_structs, phys, PAGE_SIZE,
