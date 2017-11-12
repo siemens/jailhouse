be5f8b94eb4713bece543031cefba7df430b726d
Jan Kiszka <jan.kiszka@siemens.com>
2017-11-12 11:32:50 +0100

    configs: linux-x86-demo: Make UART IRQs available to cell
    Assigned IRQ 3 and 4 of the legacy UARTs to the non-root Linux cell so
    that IRQ-driven consoles etc. work.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- configs/linux-x86-demo.c
+++ configs/linux-x86-demo.c
@@ -26,6 +26,7 @@ struct {
 	struct jailhouse_memory mem_regions[4];
 #endif
 	struct jailhouse_cache cache_regions[1];
+	struct jailhouse_irqchip irqchips[1];
 	__u8 pio_bitmap[0x2000];
 #ifdef CONFIG_QEMU_E1000E_ASSIGNMENT
 	struct jailhouse_pci_device pci_devices[2];
@@ -43,7 +44,7 @@ struct {
 		.cpu_set_size = sizeof(config.cpus),
 		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
 		.num_cache_regions = ARRAY_SIZE(config.cache_regions),
-		.num_irqchips = 0,
+		.num_irqchips = ARRAY_SIZE(config.irqchips),
 		.pio_bitmap_size = ARRAY_SIZE(config.pio_bitmap),
 		.num_pci_devices = ARRAY_SIZE(config.pci_devices),
 		.num_pci_caps = ARRAY_SIZE(config.pci_caps),
@@ -120,6 +121,16 @@ struct {
 			.start = 0,
 			.size = 2,
 			.type = JAILHOUSE_CACHE_L3,
+		},
+	},
+
+	.irqchips = {
+		/* IOAPIC */ {
+			.address = 0xfec00000,
+			.id = 0xff00,
+			.pin_bitmap = {
+				(1 << 3) | (1 << 4),
+			},
 		},
 	},
 
