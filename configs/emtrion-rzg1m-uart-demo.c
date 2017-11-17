4e01b65a74864324742e2acd49032b179525fbab
Jan von Wiarda <jan.vonwiarda@emtrion.de>
2017-11-17 11:44:19 +0100

    configs: Jailhouse support for Renesas RZ/G1M (emCON-RZ/G1M from emtrion)
    This patch adds:
    
         - root cell configuration
         - UART demo configuration
         - Linux demo configuration
         - Inmate Device Tree for emCON-RZ/G1M
    
    for Renesas RZ/G1M (emCON-RZ/G1M from emtrion).
    
    Signed-off-by: Jan von Wiarda <jan.vonwiarda@emtrion.de>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ configs/emtrion-rzg1m-uart-demo.c
@@ -0,0 +1,56 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Configuration for uart-demo inmate on emCON-RZ/G1M:
+ * 1 CPU, 64K RAM, serial ports SCIF4, CCU
+ *
+ * Copyright (c) emtrion GmbH, 2017
+ *
+ * Authors:
+ *  Jan von Wiarda <jan.vonwiarda@emtrion.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/types.h>
+#include <jailhouse/cell-config.h>
+
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+
+struct {
+	struct jailhouse_cell_desc cell;
+	__u64 cpus[1];
+	struct jailhouse_memory mem_regions[2];
+} __attribute__((packed)) config = {
+	.cell = {
+		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
+		.revision = JAILHOUSE_CONFIG_REVISION,
+		.name = "emtrion-emconrzg1m-uart-demo",
+		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
+
+		.cpu_set_size = sizeof(config.cpus),
+		.num_memory_regions = ARRAY_SIZE(config.mem_regions),
+	},
+
+	.cpus = {
+		0x2,
+	},
+
+	.mem_regions = {
+		/* SCIF4 */ {
+			.phys_start = 0xe6ee0000,
+			.virt_start = 0xe6ee0000,
+			.size = 0x1000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_IO,
+		},
+		/* RAM */ {
+			.phys_start = 0x7bff0000,
+			.virt_start = 0,
+			.size = 0x00010000,
+			.flags = JAILHOUSE_MEM_READ | JAILHOUSE_MEM_WRITE |
+				JAILHOUSE_MEM_EXECUTE | JAILHOUSE_MEM_LOADABLE,
+		},
+	}
+};
