722b74e32cf666784b2f66310c51cab50285d47c
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2018-05-07 22:46:47 +0200

    inmates: check communication region ABI version
    Or fail by setting the cell's state and halt.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- inmates/lib/setup.c
+++ inmates/lib/setup.c
@@ -4,6 +4,14 @@ void __attribute__((noreturn)) c_entry(void);
 
 void __attribute__((noreturn)) c_entry(void)
 {
-	inmate_main();
+	/* check if the ABI version of the communication region matches */
+	if (comm_region->revision != COMM_REGION_ABI_REVISION ||
+	    memcmp(comm_region->signature, COMM_REGION_MAGIC,
+		   sizeof(comm_region->signature))) {
+		comm_region->cell_state = JAILHOUSE_CELL_FAILED_COMM_REV;
+	} else {
+		inmate_main();
+	}
+
 	stop();
 }
