8f43c167814f34344716be29346227accde7631b
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2018-05-30 23:45:20 +0200

    inmates: tools: arm: linux-loader: disable MMU before starting
    Linux wants the MMU to be disabled. Disable it before handing control
    over to Linux.
    
    We need to do that in assembly, as we must not touch the stack after
    disabling the MMU.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    [Jan: add missing register clobbering, align comments and tweak wording]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- inmates/tools/arm/linux-loader.c
+++ inmates/tools/arm/linux-loader.c
@@ -12,15 +12,42 @@
  * the COPYING file in the top-level directory.
  */
 
+#include <asm/sysregs.h>
 #include <inmate.h>
 
 void inmate_main(void)
 {
 	void register (*entry)(unsigned long, unsigned long, unsigned long);
-	unsigned long register dtb;
+	unsigned long register dtb, sctlr;
 
 	entry = (void *)(unsigned long)cmdline_parse_int("kernel", 0);
 	dtb = cmdline_parse_int("dtb", 0);
 
-	entry(0, -1, dtb);
+	/*
+	 * Linux wants the MMU and D-caches to be disabled.
+	 * As we didn't write anything relevant to the caches so far, we can
+	 * get away without flushing.
+	 */
+	arm_read_sysreg(SCTLR, sctlr);
+	sctlr &= ~(SCTLR_C | SCTLR_M);
+
+	/*
+	 * This is a pendant for
+	 *   arm_write_sysreg(SCTLR, sctlr);
+	 *   instruction_barrier();
+	 *   entry(0, -1, dtb);
+	 *
+	 * After disabling the MMU, we must not touch the stack because we don't
+	 * flush+inval dcaches, and the compiler might use the stack between
+	 * calling entry. Assembly ensures that everything relevant is kept in
+	 * registers.
+	 */
+	asm volatile(
+		"mov r0, #0\n\t"
+		"mov r1, #-1\n\t"
+		"mov r2, %0\n\t"
+		"mcr p15, 0, %1, c1, c0, 0\n\t"
+		"isb\n\t"
+		"bx %2\n\t" /* entry(0, -1, dtb) */
+		: : "r" (dtb), "r" (sctlr), "r" (entry) : "r0", "r1", "r2");
 }
--- inmates/tools/arm64/linux-loader.c
+++ inmates/tools/arm64/linux-loader.c
@@ -10,15 +10,44 @@
  * the COPYING file in the top-level directory.
  */
 
+#include <asm/sysregs.h>
 #include <inmate.h>
 
 void inmate_main(void)
 {
-	unsigned long dtb;
+	unsigned long dtb, sctlr;
 	void (*entry)(u64 dtb, u64 x1, u64 x2, u64 x3);
 
 	entry = (void *)cmdline_parse_int("kernel", 0);
 	dtb = cmdline_parse_int("dtb", 0);
 
-	entry(dtb, 0, 0, 0);
+	/*
+	 * Linux wants the MMU to be disabled
+	 * As we didn't write anything relevant to the caches so far, we can
+	 * get away without flushing.
+	 */
+	arm_read_sysreg(SCTLR_EL1, sctlr);
+	sctlr &= ~SCTLR_EL1_M;
+
+	/*
+	 * This is a pendant for
+	 *   arm_write_sysreg(SCTLR_EL1, sctlr);
+	 *   instruction_barrier();
+	 *   entry(dtb, 0, 0, 0);
+	 *
+	 * After disabling the MMU, we must not touch the stack because we don't
+	 * flush+inval dcaches, and the compiler might use the stack between
+	 * calling entry. Assembly ensures that everything relevant is kept in
+	 * registers.
+	 */
+	asm volatile(
+		"mov x0, %0\n\t"
+		"mov x1, #0\n\t"
+		"mov x2, #0\n\t"
+		"mov x3, #0\n\t"
+		"msr sctlr_el1, %1\n\t"
+		"isb\n\t"
+		"br %2" /* entry(dtb, 0, 0, 0) */
+		: : "r" (dtb), "r" (sctlr), "r" (entry)
+		: "x0", "x1", "x2", "x3");
 }
