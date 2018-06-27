f2e01f5dcfbf7b5947a9793f545fbb4bc0f28a87
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2018-06-27 16:17:56 +0200

    inmates: arm-common: Define mmio_read64 accessor
    We need this accessor for reading GIC's TYPER.
    
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- inmates/lib/arm-common/include/inmate.h
+++ inmates/lib/arm-common/include/inmate.h
@@ -84,6 +84,11 @@ static inline void mmio_write32(void *address, u32 value)
 	*(volatile u32 *)address = value;
 }
 
+static inline u64 mmio_read64(void *address)
+{
+	return *(volatile u64 *)address;
+}
+
 static inline void __attribute__((noreturn)) halt(void)
 {
 	while (1)
