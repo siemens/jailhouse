siemens/jailhouse

commit 1f77840325318ffe2237ff66462c550f03d1ff84

core: Add support for creating page tables with hugepages
When adding support for generating hugepages during page_map_create, we
also need to address the issue of overwriting or splitting up such
pages. When partially unmapping a hugepage, we need to break it up
first, then unmap the included pages. This break-up may fail when we are
short on memory, thus page_map_destroy may actually fail now, and we
have to take this into account on the caller side.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/paging.c
@@ -134,6 +134,33 @@ static void flush_pt_entry(pt_entry_t pte, enum page_map_coherent coherent)
 		flush_cache(pte, sizeof(*pte));
 }
 
+static int split_hugepage(const struct paging *paging, pt_entry_t pte,
+			  unsigned long virt, enum page_map_coherent coherent)
+{
+	unsigned long phys = paging->get_phys(pte, virt);
+	struct paging_structures sub_structs;
+	unsigned long page_mask, flags;
+
+	if (phys == INVALID_PHYS_ADDR)
+		return 0;
+
+	page_mask = ~(paging->page_size - 1);
+	phys &= page_mask;
+	virt &= page_mask;
+
+	flags = paging->get_flags(pte);
+
+	sub_structs.root_paging = paging + 1;
+	sub_structs.root_table = page_alloc(&mem_pool, 1);
+	if (!sub_structs.root_table)
+		return -ENOMEM;
+	paging->set_next_pt(pte, page_map_hvirt2phys(sub_structs.root_table));
+	flush_pt_entry(pte, coherent);
+
+	return page_map_create(&sub_structs, phys, paging->page_size, virt,
+			       flags, coherent);
+}
+
 int page_map_create(const struct paging_structures *pg_structs,
 		    unsigned long phys, unsigned long size, unsigned long virt,
 		    unsigned long flags, enum page_map_coherent coherent)
@@ -145,15 +172,32 @@ int page_map_create(const struct paging_structures *pg_structs,
 		const struct paging *paging = pg_structs->root_paging;
 		page_table_t pt = pg_structs->root_table;
 		pt_entry_t pte;
+		int err;
 
 		while (1) {
 			pte = paging->get_entry(pt, virt);
-			if (paging->page_size > 0) {
+			if (paging->page_size > 0 &&
+			    paging->page_size <= size &&
+			    (virt & (paging->page_size - 1)) == 0) {
+				/*
+				 * We might be overwriting a more fine-grained
+				 * mapping, so release it first. This cannot
+				 * fail as we are working along hugepage
+				 * boundaries.
+				 */
+				if (paging->page_size > PAGE_SIZE)
+					page_map_destroy(pg_structs, virt,
+							 paging->page_size,
+							 coherent);
 				paging->set_terminal(pte, phys, flags);
 				flush_pt_entry(pte, coherent);
 				break;
 			}
 			if (paging->entry_valid(pte)) {
+				err = split_hugepage(paging, pte, virt,
+						     coherent);
+				if (err)
+					return err;
 				pt = page_map_phys2hvirt(
 						paging->get_next_pt(pte));
 			} else {
@@ -175,9 +219,9 @@ int page_map_create(const struct paging_structures *pg_structs,
 	return 0;
 }
 
-void page_map_destroy(const struct paging_structures *pg_structs,
-		      unsigned long virt, unsigned long size,
-		      enum page_map_coherent coherent)
+int page_map_destroy(const struct paging_structures *pg_structs,
+		     unsigned long virt, unsigned long size,
+		     enum page_map_coherent coherent)
 {
 	size = PAGE_ALIGN(size);
 
@@ -187,6 +231,7 @@ void page_map_destroy(const struct paging_structures *pg_structs,
 		unsigned long page_size;
 		pt_entry_t pte;
 		int n = 0;
+		int err;
 
 		/* walk down the page table, saving intermediate tables */
 		pt[0] = pg_structs->root_table;
@@ -194,8 +239,15 @@ void page_map_destroy(const struct paging_structures *pg_structs,
 			pte = paging->get_entry(pt[n], virt);
 			if (!paging->entry_valid(pte))
 				break;
-			if (paging->get_phys(pte, virt) != INVALID_PHYS_ADDR)
-				break;
+			if (paging->get_phys(pte, virt) != INVALID_PHYS_ADDR) {
+				if (paging->page_size > size) {
+					err = split_hugepage(paging, pte, virt,
+							     coherent);
+					if (err)
+						return err;
+				} else
+					break;
+			}
 			pt[++n] = page_map_phys2hvirt(
 					paging->get_next_pt(pte));
 			paging++;
@@ -220,6 +272,7 @@ void page_map_destroy(const struct paging_structures *pg_structs,
 		virt += page_size;
 		size -= page_size;
 	}
+	return 0;
 }
 
 void *page_map_get_guest_page(struct per_cpu *cpu_data,

hypervisor/arch/x86/control.c
@@ -31,14 +31,17 @@ int arch_cell_create(struct per_cpu *cpu_data, struct cell *cell)
 
 	/* TODO: Implement proper roll-backs on errors */
 
-	vmx_linux_cell_shrink(cell->config);
+	err = vmx_linux_cell_shrink(cell->config);
+	if (err)
+		return err;
 	flush_linux_cpu_caches(cpu_data);
 	err = vmx_cell_init(cell);
 	if (err)
 		return err;
 
-	vtd_linux_cell_shrink(cell->config);
-	err = vtd_cell_init(cell);
+	err = vtd_linux_cell_shrink(cell->config);
+	if (!err)
+		err = vtd_cell_init(cell);
 	if (err)
 		vmx_cell_exit(cell);
 	return err;

hypervisor/arch/x86/include/asm/vmx.h
@@ -285,7 +285,7 @@ enum vmcs_field {
 int vmx_init(void);
 
 int vmx_cell_init(struct cell *cell);
-void vmx_linux_cell_shrink(struct jailhouse_cell_desc *config);
+int vmx_linux_cell_shrink(struct jailhouse_cell_desc *config);
 int vmx_map_memory_region(struct cell *cell,
 			  const struct jailhouse_memory *mem);
 void vmx_unmap_memory_region(struct cell *cell,

hypervisor/arch/x86/include/asm/vtd.h
@@ -108,7 +108,7 @@ struct vtd_entry {
 int vtd_init(void);
 
 int vtd_cell_init(struct cell *cell);
-void vtd_linux_cell_shrink(struct jailhouse_cell_desc *config);
+int vtd_linux_cell_shrink(struct jailhouse_cell_desc *config);
 int vtd_map_memory_region(struct cell *cell,
 			  const struct jailhouse_memory *mem);
 void vtd_unmap_memory_region(struct cell *cell,

hypervisor/arch/x86/vmx.c
@@ -257,6 +257,8 @@ int vmx_map_memory_region(struct cell *cell,
 void vmx_unmap_memory_region(struct cell *cell,
 			     const struct jailhouse_memory *mem)
 {
+	/* This cannot fail. The region was mapped as a whole before, thus no
+	 * hugepages need to be broken up to unmap it. */
 	page_map_destroy(&cell->vmx.ept_structs, mem->virt_start, mem->size,
 			 PAGE_MAP_NON_COHERENT);
 }
@@ -312,26 +314,32 @@ int vmx_cell_init(struct cell *cell)
 	return 0;
 }
 
-void vmx_linux_cell_shrink(struct jailhouse_cell_desc *config)
+int vmx_linux_cell_shrink(struct jailhouse_cell_desc *config)
 {
 	const struct jailhouse_memory *mem =
 		jailhouse_cell_mem_regions(config);
 	const u8 *pio_bitmap = jailhouse_cell_pio_bitmap(config);
 	u32 pio_bitmap_size = config->pio_bitmap_size;
+	int err = 0;
 	u8 *b;
 	int n;
 
 	for (n = 0; n < config->num_memory_regions; n++, mem++)
-		if (!(mem->flags & JAILHOUSE_MEM_COMM_REGION))
-			page_map_destroy(&linux_cell.vmx.ept_structs,
-					 mem->phys_start, mem->size,
-					 PAGE_MAP_NON_COHERENT);
+		if (!(mem->flags & JAILHOUSE_MEM_COMM_REGION)) {
+			err = page_map_destroy(&linux_cell.vmx.ept_structs,
+					       mem->phys_start, mem->size,
+					       PAGE_MAP_NON_COHERENT);
+			if (err)
+				goto out;
+		}
 
 	for (b = linux_cell.vmx.io_bitmap; pio_bitmap_size > 0;
 	     b++, pio_bitmap++, pio_bitmap_size--)
 		*b |= ~*pio_bitmap;
 
+out:
 	vmx_invept();
+	return err;
 }
 
 void vmx_cell_exit(struct cell *cell)

hypervisor/arch/x86/vtd.c
@@ -287,24 +287,30 @@ vtd_remove_device_from_cell(struct cell *cell,
 	page_free(&mem_pool, context_entry_table, 1);
 }
 
-void vtd_linux_cell_shrink(struct jailhouse_cell_desc *config)
+int vtd_linux_cell_shrink(struct jailhouse_cell_desc *config)
 {
 	const struct jailhouse_memory *mem =
 		jailhouse_cell_mem_regions(config);
 	const struct jailhouse_pci_device *dev =
 		jailhouse_cell_pci_devices(config);
 	unsigned int n;
+	int err = 0;
 
 	for (n = 0; n < config->num_memory_regions; n++, mem++)
-		if (mem->flags & JAILHOUSE_MEM_DMA)
-			page_map_destroy(&linux_cell.vtd.pg_structs,
-					 mem->phys_start, mem->size,
-					 PAGE_MAP_COHERENT);
+		if (mem->flags & JAILHOUSE_MEM_DMA) {
+			err = page_map_destroy(&linux_cell.vtd.pg_structs,
+					       mem->phys_start, mem->size,
+					       PAGE_MAP_COHERENT);
+			if (err)
+				goto out;
+		}
 
 	for (n = 0; n < config->num_pci_devices; n++)
 		vtd_remove_device_from_cell(&linux_cell, &dev[n]);
 
+out:
 	vtd_flush_domain_caches(linux_cell.id);
+	return err;
 }
 
 int vtd_map_memory_region(struct cell *cell,
@@ -337,6 +343,8 @@ void vtd_unmap_memory_region(struct cell *cell,
 		return;
 
 	if (mem->flags & JAILHOUSE_MEM_DMA)
+		/* This cannot fail. The region was mapped as a whole before,
+		 * thus no hugepages need to be broken up to unmap it. */
 		page_map_destroy(&cell->vtd.pg_structs, mem->virt_start,
 				 mem->size, PAGE_MAP_COHERENT);
 }

hypervisor/include/jailhouse/paging.h
@@ -109,9 +109,9 @@ unsigned long arch_page_map_gphys2phys(struct per_cpu *cpu_data,
 int page_map_create(const struct paging_structures *pg_structs,
 		    unsigned long phys, unsigned long size, unsigned long virt,
 		    unsigned long flags, enum page_map_coherent coherent);
-void page_map_destroy(const struct paging_structures *pg_structs,
-		      unsigned long virt, unsigned long size,
-		      enum page_map_coherent coherent);
+int page_map_destroy(const struct paging_structures *pg_structs,
+		     unsigned long virt, unsigned long size,
+		     enum page_map_coherent coherent);
 
 void *page_map_get_guest_page(struct per_cpu *cpu_data,
 			      const struct paging *paging,

commit 932556921b07121e6ce19573ba3efa97615a41ff

core: Fix mask calculation of split_hugepage
Long pending issue: page_size is only unsigned int, but we need long
when dealing with 64-bit addresses that have high bits set.

Fixes: 1f7784032531 ("core: Add support for creating page tables with hugepages")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/paging.c
@@ -250,7 +250,7 @@ static int split_hugepage(const struct paging *paging, pt_entry_t pte,
 	if (phys == INVALID_PHYS_ADDR)
 		return 0;
 
-	page_mask = ~(paging->page_size - 1);
+	page_mask = ~((unsigned long)paging->page_size - 1);
 	phys &= page_mask;
 	virt &= page_mask;
 