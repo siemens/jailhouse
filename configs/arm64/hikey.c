d112438838a96fda8b1eafd21da4d264c5137cea
Jan Kiszka <jan.kiszka@siemens.com>
2018-09-17 08:04:13 +0200

    configs: Use fixed PCI domain for hikey
    This board has no other PCI controllers, so we can just use the default
    domain 0.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- configs/arm64/hikey.c
+++ configs/arm64/hikey.c
@@ -43,7 +43,6 @@ struct {
 			.pci_mmconfig_base = 0xf6000000,
 			.pci_mmconfig_end_bus = 0,
 			.pci_is_virtual = 1,
-			.pci_domain = -1,
 			.arm = {
 				.gic_version = 2,
 				.gicd_base = 0xf6801000,
