siemens/jailhouse

commit 7688e96c815ba2f7c713228ecdd75241bcb8102a

arm-common: Rework handling of SMC
Latest versions of SMC have more services to handle
other than PSCI and SIP. In order to handle newer versions
and not to clutter smc handling for arm and arm64 separately,
consolidating the SMC handling into arm-common/smccc.c.
This is similar to the method implemented in kernel.

Signed-off-by: Lokesh Vutla <lokeshvutla@ti.com>
Tested-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
[Jan: reformatted header of new files,
      added missing instruction skip to arch_handle_smc]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/arm-common/smccc.c
@@ -1,0 +1,39 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * Authors:
+ *  Lokesh Vutla <lokeshvutla@ti.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/control.h>
+#include <asm/psci.h>
+#include <asm/traps.h>
+#include <asm/smccc.h>
+
+int handle_smc(struct trap_context *ctx)
+{
+	unsigned long *regs = ctx->regs;
+
+	switch (SMCCC_GET_OWNER(regs[0])) {
+	case ARM_SMCCC_OWNER_SIP:
+		regs[0] = ARM_SMCCC_NOT_SUPPORTED;
+		break;
+
+	case ARM_SMCCC_OWNER_STANDARD:
+		regs[0] = psci_dispatch(ctx);
+		break;
+
+	default:
+		return TRAP_UNHANDLED;
+
+	}
+
+	arch_skip_instruction(ctx);
+
+	return TRAP_HANDLED;
+}

hypervisor/arch/arm-common/Kbuild
@@ -17,6 +17,6 @@ ccflags-$(CONFIG_JAILHOUSE_GCOV) += -fprofile-arcs -ftest-coverage
 objs-y += dbg-write.o lib.o psci.o control.o paging.o mmu_cell.o setup.o
 objs-y += irqchip.o pci.o ivshmem.o uart-pl011.o uart-xuartps.o uart-mvebu.o
 objs-y += uart-hscif.o uart-scifa.o uart-imx.o
-objs-y += gic-v2.o gic-v3.o
+objs-y += gic-v2.o gic-v3.o smccc.o
 
 common-objs-y = $(addprefix ../arm-common/,$(objs-y))

hypervisor/arch/arm-common/include/asm/psci.h
@@ -32,8 +32,6 @@
 #define PSCI_CPU_IS_ON			0
 #define PSCI_CPU_IS_OFF			1
 
-#define IS_PSCI_32(hvc)			(((hvc) >> 24) == 0x84)
-#define IS_PSCI_64(hvc)			(((hvc) >> 24) == 0xc4)
 #define IS_PSCI_UBOOT(hvc)		(((hvc) >> 8) == 0x95c1ba)
 
 #define PSCI_INVALID_ADDRESS		(-1L)

hypervisor/arch/arm-common/include/asm/sip.h
@@ -1,15 +1,0 @@
-/*
- * Jailhouse, a Linux-based partitioning hypervisor
- *
- * Authors:
- *  Peng Fang <peng.fan@nxp.com>
- *  Claudio Scordino <claudio@evidence.eu.com>
- *
- * This work is licensed under the terms of the GNU GPL, version 2.  See
- * the COPYING file in the top-level directory.
- */
-
-#define IS_SIP_32(hvc)			(((hvc) >> 24) == 0x82)
-#define IS_SIP_64(hvc)			(((hvc) >> 24) == 0xc2)
-
-#define SIP_NOT_SUPPORTED		(-1)

hypervisor/arch/arm-common/include/asm/smccc.h
@@ -1,0 +1,30 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (C) 2018 Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * Authors:
+ *  Lokesh Vutla <lokeshvutla@ti.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#define ARM_SMCCC_OWNER_MASK		BIT_MASK(29, 24)
+#define ARM_SMCCC_OWNER_SHIFT		24
+
+#define ARM_SMCCC_OWNER_SIP             2
+#define ARM_SMCCC_OWNER_STANDARD        4
+
+#define ARM_SMCCC_CONV_32		0
+#define ARM_SMCCC_CONV_64		1
+
+#define ARM_SMCCC_NOT_SUPPORTED         (-1)
+#define ARM_SMCCC_SUCCESS               0
+
+#define SMCCC_GET_OWNER(id)	((id & ARM_SMCCC_OWNER_MASK) >>	\
+				 ARM_SMCCC_OWNER_SHIFT)
+
+#define SMCCC_IS_CONV_64(function_id)	!!(function_id & (1 << 30))
+
+int handle_smc(struct trap_context *ctx);

hypervisor/arch/arm-common/psci.c
@@ -15,11 +15,12 @@
 #include <jailhouse/control.h>
 #include <asm/control.h>
 #include <asm/psci.h>
+#include <asm/smccc.h>
 #include <asm/traps.h>
 
 static long psci_emulate_cpu_on(struct trap_context *ctx)
 {
-	unsigned long mask = IS_PSCI_64(ctx->regs[0]) ? (u64)-1L : (u32)-1;
+	u64 mask = SMCCC_IS_CONV_64(ctx->regs[0]) ? (u64)-1L : (u32)-1;
 	struct public_per_cpu *target_data;
 	bool kick_cpu = false;
 	unsigned int cpu;

hypervisor/arch/arm/traps.c
@@ -19,6 +19,7 @@
 #include <asm/control.h>
 #include <asm/gic.h>
 #include <asm/psci.h>
+#include <asm/smccc.h>
 #include <asm/traps.h>
 #include <asm/sysregs.h>
 
@@ -248,14 +249,16 @@ static int arch_handle_smc(struct trap_context *ctx)
 {
 	unsigned long *regs = ctx->regs;
 
-	if (!IS_PSCI_32(regs[0]) && !IS_PSCI_UBOOT(regs[0]))
+	if (SMCCC_IS_CONV_64(regs[0]))
 		return TRAP_FORBIDDEN;
 
-	regs[0] = psci_dispatch(ctx);
+	if (IS_PSCI_UBOOT(regs[0])) {
+		regs[0] = psci_dispatch(ctx);
+		arch_skip_instruction(ctx);
+		return TRAP_HANDLED;
+	}
 
-	arch_skip_instruction(ctx);
-
-	return TRAP_HANDLED;
+	return handle_smc(ctx);
 }
 
 static int arch_handle_hvc(struct trap_context *ctx)

hypervisor/arch/arm64/traps.c
@@ -18,7 +18,7 @@
 #include <asm/gic.h>
 #include <asm/mmio.h>
 #include <asm/psci.h>
-#include <asm/sip.h>
+#include <asm/smccc.h>
 #include <asm/sysregs.h>
 #include <asm/traps.h>
 #include <asm/processor.h>
@@ -31,24 +31,6 @@ void arch_skip_instruction(struct trap_context *ctx)
 	arm_read_sysreg(ELR_EL2, pc);
 	pc += ESR_IL(ctx->esr) ? 4 : 2;
 	arm_write_sysreg(ELR_EL2, pc);
-}
-
-static int handle_smc(struct trap_context *ctx)
-{
-	unsigned long *regs = ctx->regs;
-
-	if (IS_PSCI_32(regs[0]) || IS_PSCI_64(regs[0])) {
-		regs[0] = psci_dispatch(ctx);
-	} else if (IS_SIP_32(regs[0]) || IS_SIP_64(regs[0])) {
-		/* This can be ignored */
-		regs[0] = SIP_NOT_SUPPORTED;
-	} else {
-		return TRAP_UNHANDLED;
-	}
-
-	arch_skip_instruction(ctx);
-
-	return TRAP_HANDLED;
 }
 
 static int handle_hvc(struct trap_context *ctx)

commit 0ef181543fcc3ff47f5f6f502ff88d351304feca

arm-common: account for SMC exits
Statistics on ARM currently has some imbalances: the total number of
exits doesn't equal the sum of the fine granular exit counters: we
aren't accounting for SMCCC exits.

Fix this by adding a new statistic counter for SMCCC.

PSCI exits are already accounted inside psci_dispatch(), move SMCCC
accounting to the dispatcher routine handle_smc().

Fixes: 7688e96c815b ("arm-common: Rework handling of SMC")
Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

driver/sysfs.c
@@ -148,6 +148,7 @@ JAILHOUSE_CPU_STATS_ATTR(vmexits_maintenance,
 JAILHOUSE_CPU_STATS_ATTR(vmexits_virt_irq, JAILHOUSE_CPU_STAT_VMEXITS_VIRQ);
 JAILHOUSE_CPU_STATS_ATTR(vmexits_virt_sgi, JAILHOUSE_CPU_STAT_VMEXITS_VSGI);
 JAILHOUSE_CPU_STATS_ATTR(vmexits_psci, JAILHOUSE_CPU_STAT_VMEXITS_PSCI);
+JAILHOUSE_CPU_STATS_ATTR(vmexits_smccc, JAILHOUSE_CPU_STAT_VMEXITS_SMCCC);
 #ifdef CONFIG_ARM
 JAILHOUSE_CPU_STATS_ATTR(vmexits_cp15, JAILHOUSE_CPU_STAT_VMEXITS_CP15);
 #endif
@@ -172,6 +173,7 @@ static struct attribute *cell_stats_attrs[] = {
 	&vmexits_virt_irq_cell_attr.kattr.attr,
 	&vmexits_virt_sgi_cell_attr.kattr.attr,
 	&vmexits_psci_cell_attr.kattr.attr,
+	&vmexits_smccc_cell_attr.kattr.attr,
 #ifdef CONFIG_ARM
 	&vmexits_cp15_cell_attr.kattr.attr,
 #endif
@@ -203,6 +205,7 @@ static struct attribute *cpu_stats_attrs[] = {
 	&vmexits_virt_irq_cpu_attr.kattr.attr,
 	&vmexits_virt_sgi_cpu_attr.kattr.attr,
 	&vmexits_psci_cpu_attr.kattr.attr,
+	&vmexits_smccc_cpu_attr.kattr.attr,
 #ifdef CONFIG_ARM
 	&vmexits_cp15_cpu_attr.kattr.attr,
 #endif

hypervisor/arch/arm-common/smccc.c
@@ -33,13 +33,16 @@ static long handle_arch(struct trap_context *ctx)
 int handle_smc(struct trap_context *ctx)
 {
 	unsigned long *regs = ctx->regs;
+	u32 *stats = this_cpu_public()->stats;
 
 	switch (SMCCC_GET_OWNER(regs[0])) {
 	case ARM_SMCCC_OWNER_ARCH:
+		stats[JAILHOUSE_CPU_STAT_VMEXITS_SMCCC]++;
 		regs[0] = handle_arch(ctx);
 		break;
 
 	case ARM_SMCCC_OWNER_SIP:
+		stats[JAILHOUSE_CPU_STAT_VMEXITS_SMCCC]++;
 		regs[0] = ARM_SMCCC_NOT_SUPPORTED;
 		break;
 

include/arch/arm/asm/jailhouse_hypercall.h
@@ -51,8 +51,9 @@
 #define JAILHOUSE_CPU_STAT_VMEXITS_VIRQ		JAILHOUSE_GENERIC_CPU_STATS + 1
 #define JAILHOUSE_CPU_STAT_VMEXITS_VSGI		JAILHOUSE_GENERIC_CPU_STATS + 2
 #define JAILHOUSE_CPU_STAT_VMEXITS_PSCI		JAILHOUSE_GENERIC_CPU_STATS + 3
-#define JAILHOUSE_CPU_STAT_VMEXITS_CP15		JAILHOUSE_GENERIC_CPU_STATS + 4
-#define JAILHOUSE_NUM_CPU_STATS			JAILHOUSE_GENERIC_CPU_STATS + 5
+#define JAILHOUSE_CPU_STAT_VMEXITS_SMCCC	JAILHOUSE_GENERIC_CPU_STATS + 4
+#define JAILHOUSE_CPU_STAT_VMEXITS_CP15		JAILHOUSE_GENERIC_CPU_STATS + 5
+#define JAILHOUSE_NUM_CPU_STATS			JAILHOUSE_GENERIC_CPU_STATS + 6
 
 #ifndef __ASSEMBLY__
 

include/arch/arm64/asm/jailhouse_hypercall.h
@@ -50,7 +50,8 @@
 #define JAILHOUSE_CPU_STAT_VMEXITS_VIRQ		JAILHOUSE_GENERIC_CPU_STATS + 1
 #define JAILHOUSE_CPU_STAT_VMEXITS_VSGI		JAILHOUSE_GENERIC_CPU_STATS + 2
 #define JAILHOUSE_CPU_STAT_VMEXITS_PSCI		JAILHOUSE_GENERIC_CPU_STATS + 3
-#define JAILHOUSE_NUM_CPU_STATS			JAILHOUSE_GENERIC_CPU_STATS + 4
+#define JAILHOUSE_CPU_STAT_VMEXITS_SMCCC	JAILHOUSE_GENERIC_CPU_STATS + 4
+#define JAILHOUSE_NUM_CPU_STATS			JAILHOUSE_GENERIC_CPU_STATS + 5
 
 #ifndef __ASSEMBLY__
 