03d09b9c3b119ea704edafdd9f358ce57e2a829f
Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
2019-01-03 19:06:45 +0100

    arm, arm64: add stubs for SMC calls
    Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ hypervisor/arch/arm/include/asm/smc.h
@@ -0,0 +1,38 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (C) 2018 OTH Regensburg
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+asm (".arch_extension sec\n");
+
+static inline int smc(unsigned long id)
+{
+	register unsigned long __id asm("r0") = id;
+
+	asm volatile ("smc #0\n\t"
+		: "=r" (__id)
+		: "r"(__id)
+		: "memory", "r1", "r2", "r3");
+
+	return __id;
+}
+
+static inline int smc_arg1(unsigned long id, unsigned long par1)
+{
+	register unsigned long __id asm("r0") = id;
+	register unsigned long __par1 asm("r1") = par1;
+
+	asm volatile ("smc #0\n\t"
+		: "=r" (__id)
+		: "r"(__id), "r"(__par1)
+		: "memory", "r2", "r3");
+
+	return __id;
+}
---
+++ hypervisor/arch/arm64/include/asm/smc.h
@@ -0,0 +1,36 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (C) 2018 OTH Regensburg
+ *
+ * Authors:
+ *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+static inline int smc(unsigned long id)
+{
+	register unsigned long __id asm("r0") = id;
+
+	asm volatile ("smc #0\n\t"
+		: "=r" (__id)
+		: "r"(__id)
+		: "memory", "x1", "x2", "x3");
+
+	return __id;
+}
+
+static inline int smc_arg1(unsigned long id, unsigned long par1)
+{
+	register unsigned long __id asm("r0") = id;
+	register unsigned long __par1 asm("r1") = par1;
+
+	asm volatile ("smc #0\n\t"
+		: "=r" (__id)
+		: "r"(__id), "r"(__par1)
+		: "memory", "x2", "x3");
+
+	return __id;
+}
