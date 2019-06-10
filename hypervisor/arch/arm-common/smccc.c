siemens/jailhouse

commit ea924a3fec98684863f6abbdd2860d1ae5c1ea53

arm64: Initialise SMCCC backend
by discovering its features.

The first step is to check the PSCI version. Don't even try to do any
SMCCC calls without having checked the proper PSCI version (current QEMU
horribly crashes).

Probe if SMCCC_ARCH_FEATURES is available. If so, probe for
SMCCC_ARCH_WORKAROUND_1 and expose its availability by setting a flag
inside the percpu structure.

The availability is stored per-cpu, as we might have big.LITTLE systems,
where only a subset of cores need mitigations.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/arm-common/smccc.c
@@ -11,9 +11,41 @@
  */
 
 #include <jailhouse/control.h>
+#include <jailhouse/printk.h>
 #include <asm/psci.h>
 #include <asm/traps.h>
+#include <asm/smc.h>
 #include <asm/smccc.h>
+
+void smccc_discover(void)
+{
+	int ret;
+
+	ret = smc(PSCI_0_2_FN_VERSION);
+
+	/* We need >=PSCIv1.0 for SMCCC */
+	if (PSCI_VERSION_MAJOR(ret) < 1)
+		return;
+
+	/* Check if PSCI supports SMCCC version call */
+	ret = smc_arg1(PSCI_1_0_FN_FEATURES, SMCCC_VERSION);
+	if (ret != ARM_SMCCC_SUCCESS)
+		return;
+
+	/* We need to have SMCCC v1.1 */
+	ret = smc(SMCCC_VERSION);
+	if (ret != ARM_SMCCC_VERSION_1_1)
+		return;
+
+	/* check if SMCCC_ARCH_FEATURES is actually available */
+	ret = smc_arg1(SMCCC_ARCH_FEATURES, SMCCC_ARCH_FEATURES);
+	if (ret != ARM_SMCCC_SUCCESS)
+		return;
+
+	ret = smc_arg1(SMCCC_ARCH_FEATURES, SMCCC_ARCH_WORKAROUND_1);
+
+	this_cpu_data()->smccc_has_workaround_1 = ret >= ARM_SMCCC_SUCCESS;
+}
 
 static long handle_arch(struct trap_context *ctx)
 {

hypervisor/arch/arm-common/include/asm/percpu.h
@@ -15,6 +15,9 @@
 
 #define STACK_SIZE			PAGE_SIZE
 
+#define ARM_PERCPU_FIELDS						\
+	bool smccc_has_workaround_1;
+
 #define ARCH_PUBLIC_PERCPU_FIELDS					\
 	unsigned long mpidr;						\
 									\

hypervisor/arch/arm-common/include/asm/smccc.h
@@ -12,6 +12,8 @@
 
 #define SMCCC_VERSION			0x80000000
 #define SMCCC_ARCH_FEATURES		0x80000001
+#define SMCCC_ARCH_WORKAROUND_1		0x80008000
+#define SMCCC_ARCH_WORKAROUND_2		0x80007fff
 
 #define ARM_SMCCC_OWNER_MASK		BIT_MASK(29, 24)
 #define ARM_SMCCC_OWNER_SHIFT		24
@@ -33,4 +35,7 @@
 
 #define SMCCC_IS_CONV_64(function_id)	!!(function_id & (1 << 30))
 
+struct trap_context;
+
+void smccc_discover(void);
 enum trap_return handle_smc(struct trap_context *ctx);

hypervisor/arch/arm-common/setup.c
@@ -14,6 +14,7 @@
 #include <jailhouse/paging.h>
 #include <jailhouse/processor.h>
 #include <asm/setup.h>
+#include <asm/smccc.h>
 
 static u32 __attribute__((aligned(PAGE_SIZE))) parking_code[PAGE_SIZE / 4] = {
 	ARM_PARKING_CODE
@@ -41,6 +42,8 @@ int arm_cpu_init(struct per_cpu *cpu_data)
 	cpu_data->public.mpidr = phys_processor_id();
 
 	arm_paging_vcpu_init(&root_cell.arch.mm);
+
+	smccc_discover();
 
 	return irqchip_cpu_init(cpu_data);
 }

hypervisor/arch/arm/include/asm/percpu_fields.h
@@ -13,6 +13,7 @@
 #define NUM_ENTRY_REGS			13
 
 #define ARCH_PERCPU_FIELDS						\
+	ARM_PERCPU_FIELDS						\
 	/** Linux stack pointer, used for handover to the hypervisor. */ \
 	unsigned long linux_sp;						\
 	unsigned long linux_ret;					\

hypervisor/arch/arm64/include/asm/percpu_fields.h
@@ -11,4 +11,5 @@
  */
 
 #define ARCH_PERCPU_FIELDS						\
+	ARM_PERCPU_FIELDS						\
 	unsigned long id_aa64mmfr0;

commit 6e27d3af3557c8d1bca5423aee32e828cca567c0

arm: Account for non-compliant PSCI_VERSION return codes
U-Boot returns PSCI_NOT_SUPPORTED, rather than the implemented 0.2. This
is practically harmless as the succeeding PSCI 1.0 call will then fail,
but it is cleaner to filter out all negative return codes.

Fixes: ea924a3fec98 ("arm64: Initialise SMCCC backend")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
Tested-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>

hypervisor/arch/arm-common/smccc.c
@@ -23,8 +23,9 @@ void smccc_discover(void)
 
 	ret = smc(PSCI_0_2_FN_VERSION);
 
-	/* We need >=PSCIv1.0 for SMCCC */
-	if (PSCI_VERSION_MAJOR(ret) < 1)
+	/* We need >=PSCIv1.0 for SMCCC. Against the spec, U-Boot may also
+	 * return a negative error code. */
+	if (ret < 0 || PSCI_VERSION_MAJOR(ret) < 1)
 		return;
 
 	/* Check if PSCI supports SMCCC version call */