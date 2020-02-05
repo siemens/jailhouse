siemens/jailhouse

commit 68b8ff84ae8dcf2633310bb9f448e2823c3b6df8

configs: arm/arm64: Consistently move ivshmem-net devices one PCI slot up
This free 00:00.0 on the virtual PCI controllers for the demo ivshmem
device. It should come before the networking device because it also uses
memory located before it.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/arm/bananapi-linux-demo.c
@@ -104,7 +104,7 @@ struct {
 	.pci_devices = {
 		{
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0x00,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm/jetson-tk1-linux-demo.c
@@ -105,9 +105,9 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0x00,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm64/espressobin-linux-demo.c
@@ -95,9 +95,9 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0 << 3,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm64/hikey-linux-demo.c
@@ -92,9 +92,9 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0x00,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm64/jetson-tx1-linux-demo.c
@@ -109,9 +109,9 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0x00,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm64/k3-am654-idk-linux-demo.c
@@ -54,7 +54,7 @@ struct {
 	},
 
 	.mem_regions = {
-		/* IVSHMEM shared memory region for 00:00.0 */
+		/* IVSHMEM shared memory region for 00:01.0 */
 		JAILHOUSE_SHMEM_NET_REGIONS(0x8dfb00000, 1),
 		/* RAM load */ {
 			.phys_start = 0x8FFFF0000,
@@ -133,9 +133,9 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0x00,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm64/k3-j721e-evm-linux-demo.c
@@ -54,7 +54,7 @@ struct {
 	},
 
 	.mem_regions = {
-		/* IVSHMEM shared memory region for 00:00.0 */
+		/* IVSHMEM shared memory region for 00:01.0 */
 		JAILHOUSE_SHMEM_NET_REGIONS(0x89fe00000, 1),
 		/* ctrl mmr */ {
 			.phys_start = 0x00100000,
@@ -259,7 +259,7 @@ struct {
 	.pci_devices = {
 		/* 00:00.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0x00,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

configs/arm64/macchiatobin-linux-demo.c
@@ -103,9 +103,9 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
-			.bdf = 0 << 3,
+			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,
 			.shmem_regions_start = 0,
 			.shmem_dev_id = 1,

commit 7e538dd94667a1426c4a694f8bd9c6071ac41b88

configs: arm/arm64: Fix vPCI IRQ assignment of Linux configs
By moving the ivshmem devices for virtual network one slot up, we also
changed their IRQs. Adjust the irqchip configs accordingly.

Fixes: 68b8ff84ae8d ("configs/arm/bananapi-linux-demo.c")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/arm/bananapi-linux-demo.c
@@ -96,7 +96,7 @@ struct {
 			.address = 0x01c81000,
 			.pin_base = 32,
 			.pin_bitmap = {
-				1 << (52-32), 0, 0, 1 << (155-128),
+				1 << (52-32), 0, 0, 1 << (156-128),
 			},
 		},
 	},

configs/arm/jetson-tk1-linux-demo.c
@@ -99,7 +99,7 @@ struct {
 			.address = 0x50041000,
 			.pin_base = 160,
 			.pin_bitmap = {
-				1 << (152+32 - 160),
+				1 << (153+32 - 160),
 			},
 		},
 	},

configs/arm64/espressobin-linux-demo.c
@@ -89,7 +89,7 @@ struct {
 				(1 << (43 - 32)) | (1 << (44 - 32)) | (1 << (45 - 32)),
 				0,
 				0,
-				(1 << (140 - 128))
+				(1 << (141 - 128))
 			},
 		},
 	},

configs/arm64/hikey-linux-demo.c
@@ -86,7 +86,7 @@ struct {
 			.address = 0xf6801000,
 			.pin_base = 32,
 			.pin_bitmap = {
-				0, 1 << (71 - 64), 0, 1 << (143 - 128)
+				0, 1 << (71 - 64), 0, 1 << (144 - 128)
 			},
 		},
 	},

configs/arm64/jetson-tx1-linux-demo.c
@@ -103,7 +103,7 @@ struct {
 			.address = 0x50041000,
 			.pin_base = 160,
 			.pin_bitmap = {
-				1 << (152+32 - 160),
+				1 << (153+32 - 160),
 			},
 		},
 	},

configs/arm64/k3-am654-idk-linux-demo.c
@@ -120,7 +120,7 @@ struct {
 			.address = 0x01800000,
 			.pin_base = 160,
 			.pin_bitmap = {
-			1 << (189 - 160), 0x0, 0x00, 0,
+			1 << (190 - 160), 0x0, 0x00, 0,
 			},
 		},
 		{

configs/arm64/k3-j721e-evm-linux-demo.c
@@ -259,7 +259,7 @@ struct {
 	},
 
 	.pci_devices = {
-		/* 00:00.0 */ {
+		/* 00:01.0 */ {
 			.type = JAILHOUSE_PCI_TYPE_IVSHMEM,
 			.bdf = 1 << 3,
 			.bar_mask = JAILHOUSE_IVSHMEM_BAR_MASK_INTX,

configs/arm64/macchiatobin-linux-demo.c
@@ -96,7 +96,7 @@ struct {
 			.pin_bitmap = {
 				(1 << (51 - 32)),
 				0,
-				(1 << (112 - 96)),
+				(1 << (113 - 96)),
 				0
 			},
 		},

configs/arm64/ultra96-linux-demo.c
@@ -89,7 +89,7 @@ struct {
 				1 << (54 - 32),
 				0,
 				0,
-				(1 << (140 - 128)) | (1 << (142 - 128))
+				(1 << (141 - 128)) | (1 << (142 - 128))
 			},
 		},
 	},

configs/arm64/zynqmp-zcu102-linux-demo-2.c
@@ -85,7 +85,7 @@ struct {
 				0,
 				0,
 				0,
-				(1 << (145 - 128)) | (1 << (146 - 128))
+				(1 << (146 - 128)) | (1 << (147 - 128))
 			},
 		},
 	},

configs/arm64/zynqmp-zcu102-linux-demo.c
@@ -91,7 +91,7 @@ struct {
 				1 << (54 - 32),
 				0,
 				0,
-				(1 << (140 - 128)) | (1 << (142 - 128))
+				(1 << (141 - 128)) | (1 << (143 - 128))
 			},
 		},
 	},