25787d78f88b97819c8769e296d4e1edeca68647
Jan Kiszka <jan.kiszka@siemens.com>
2020-02-15 13:27:27 +0100

    core: Reorder CR and LF on debug console
    Unix tools like awk expect Unix ordering and that is different from what
    we generate so far. Issue the CR first, and then the LF.
    
    Reported-by: Michael Hinton <michael.g.hinton@gmail.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/uart.c
+++ hypervisor/uart.c
@@ -2,9 +2,11 @@
  * Jailhouse, a Linux-based partitioning hypervisor
  *
  * Copyright (c) OTH Regensburg, 2016
+ * Copyright (c) Siemens AG, 2020
  *
  * Authors:
  *  Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
+ *  Jan Kiszka <jan.kiszka@siemens.com>
  *
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
@@ -17,22 +19,27 @@
 
 struct uart_chip *uart = NULL;
 
+static void uart_write_char(char c)
+{
+	while (uart->is_busy(uart))
+		cpu_relax();
+	if (panic_in_progress && panic_cpu != phys_processor_id())
+		return;
+	uart->write_char(uart, c);
+}
+
 void uart_write(const char *msg)
 {
-	char c = 0;
+	char c;
 
 	while (1) {
-		if (c == '\n')
-			c = '\r';
-		else
-			c = *msg++;
+		c = *msg++;
 		if (!c)
 			break;
 
-		while (uart->is_busy(uart))
-			cpu_relax();
-		if (panic_in_progress && panic_cpu != phys_processor_id())
-			break;
-		uart->write_char(uart, c);
+		if (c == '\n')
+			uart_write_char('\r');
+
+		uart_write_char(c);
 	}
 }
