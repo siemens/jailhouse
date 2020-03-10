siemens/jailhouse

commit bac03e4d5f5479933f1f5d0fbbc3f7aa6c4a0570

x86: ioapic: Rework and fix redir entry programming
Currently, EOI of level-triggered IOAPIC interrupts may not work while
the related entry is masked - at least with QEMU as machine and
PREEMPT-RT as root cell. This is because we always overwrite trigger
mode and vector information when masking an entry.

Let's use that issue to rethink the programming pattern. The new one
works by still avoiding to map while masked but writing the entry with
an invalid remapping index. Furthermore, a redirection entry is only
updated in the IOAPIC on writes to the lower 32 bits. This is to ensure
its consistency. Writing higher bits happens first, either out of masked
state or with the identical value.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/ioapic.c
@@ -114,49 +114,52 @@ static int ioapic_virt_redir_write(struct cell_ioapic *ioapic,
 	struct phys_ioapic *phys_ioapic = ioapic->phys_ioapic;
 	struct apic_irq_message irq_msg;
 	union ioapic_redir_entry entry;
-	int result;
+	int result = 0xffff;
 
 	entry = phys_ioapic->shadow_redir_table[pin];
 	entry.raw[reg & 1] = value;
 	phys_ioapic->shadow_redir_table[pin] = entry;
 
-	/* Do not map the interrupt while masked. */
-	if (entry.native.mask) {
-		/*
-		 * The mask is part of the lower 32 bits. Apply it when that
-		 * register half is written.
-		 */
-		if ((reg & 1) == 0)
-			ioapic_reg_write(phys_ioapic, reg, IOAPIC_REDIR_MASK);
-		return 0;
+	/*
+	 * Do not map the interrupt while masked. It may contain invalid state.
+	 * Rather write the invalid index 0xffff. That will not be used anyway
+	 * while the mask is set.
+	 */
+	if (!entry.native.mask) {
+		irq_msg = ioapic_translate_redir_entry(ioapic, pin, entry);
+
+		result = iommu_map_interrupt(ioapic->cell,
+					     (u16)ioapic->info->id, pin,
+					     irq_msg);
+		// HACK for QEMU
+		if (result == -ENOSYS) {
+			/* see regular update below, lazy version */
+			ioapic_reg_write(phys_ioapic, reg | 1, entry.raw[1]);
+			ioapic_reg_write(phys_ioapic, reg, entry.raw[reg & 1]);
+			return 0;
+		}
+		if (result < 0)
+			return result;
 	}
-
-	irq_msg = ioapic_translate_redir_entry(ioapic, pin, entry);
-
-	result = iommu_map_interrupt(ioapic->cell, (u16)ioapic->info->id, pin,
-				     irq_msg);
-	// HACK for QEMU
-	if (result == -ENOSYS) {
-		/* see regular update below, lazy version */
-		ioapic_reg_write(phys_ioapic, reg | 1, entry.raw[1]);
-		ioapic_reg_write(phys_ioapic, reg, entry.raw[reg & 1]);
-		return 0;
-	}
-	if (result < 0)
-		return result;
-
-	entry.remap.zero = 0;
-	entry.remap.int_index15 = result >> 15;
-	entry.remap.remapped = 1;
-	entry.remap.int_index = result;
 
 	/*
-	 * Upper 32 bits weren't written physically if the entry was masked so
-	 * far. Write them unconditionally when setting the lower bits.
+	 * Write all 64 bits on updates of the lower 32 bits to ensure the
+	 * consistency of an entry.
+	 *
+	 * The index information in the higher bits does not change when the
+	 * guest programs an entry, but they need to be initialized when taking
+	 * their ownership (always out of masked state, see
+	 * ioapic_prepare_handover).
 	 */
-	if ((reg & 1) == 0)
+	if ((reg & 1) == 0) {
+		entry.remap.zero = 0;
+		entry.remap.int_index15 = result >> 15;
+		entry.remap.remapped = 1;
+		entry.remap.int_index = result;
+
 		ioapic_reg_write(phys_ioapic, reg | 1, entry.raw[1]);
-	ioapic_reg_write(phys_ioapic, reg, entry.raw[reg & 1]);
+		ioapic_reg_write(phys_ioapic, reg, entry.raw[0]);
+	}
 
 	return 0;
 }

commit 2e4d71f66439f8f6223b129b1b8f19062a851342

x86: ioapic: Fix programming of to-be-masked pin
The unconditional writing of both redirection table words created an
invalid intermediate state when masking a previously unmasked pin: As
the entry was to be masked, the index in result was set to 0xffff. This
value was then programmed into the upper word while the pin was still
unmasked.

QEMU detected this invalid redirection table entry but only a message
was logged on the host terminal because QEMU does not emulate error
reporting for VT-d. If an interrupt had come in on real hardware right
at this point, we would have seen a VT-d fault. Still, no kitten would
have been harmed.

Fix this by only writing the upper half when we are unmasking the pin
(or keeping it unmasked). And the goal of bac03e4d5f54 is still achieved
this way.

Fixes: bac03e4d5f54 ("x86: ioapic: Rework and fix redir entry programming")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/ioapic.c
@@ -143,12 +143,12 @@ static int ioapic_virt_redir_write(struct cell_ioapic *ioapic,
 	}
 
 	/*
-	 * Write all 64 bits on updates of the lower 32 bits to ensure the
-	 * consistency of an entry.
+	 * Write all 64 bits on updates of the lower 32 bits when unmasked to
+	 * ensure the consistency of an entry.
 	 *
 	 * The index information in the higher bits does not change when the
-	 * guest programs an entry, but they need to be initialized when taking
-	 * their ownership (always out of masked state, see
+	 * guest programs an unmasked entry, but they need to be initialized
+	 * when taking their ownership (always out of masked state, see
 	 * ioapic_prepare_handover).
 	 */
 	if ((reg & 1) == 0) {
@@ -157,7 +157,8 @@ static int ioapic_virt_redir_write(struct cell_ioapic *ioapic,
 		entry.remap.remapped = 1;
 		entry.remap.int_index = result;
 
-		ioapic_reg_write(phys_ioapic, reg | 1, entry.raw[1]);
+		if (!entry.native.mask)
+			ioapic_reg_write(phys_ioapic, reg | 1, entry.raw[1]);
 		ioapic_reg_write(phys_ioapic, reg, entry.raw[0]);
 	}
 