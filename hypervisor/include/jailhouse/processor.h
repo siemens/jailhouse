614c2347ea5528ce9ed42c455c544d3c09219a94
Andrea Bastoni <andrea.bastoni@tum.de>
2020-10-22 19:58:00 +0200

    hypervisor: protect inclusion of processor.h
    Signed-off-by: Andrea Bastoni <andrea.bastoni@tum.de>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- hypervisor/include/jailhouse/processor.h
+++ hypervisor/include/jailhouse/processor.h
@@ -9,7 +9,11 @@
  * This work is licensed under the terms of the GNU GPL, version 2.  See
  * the COPYING file in the top-level directory.
  */
+#ifndef _JAILHOUSE_PROCESSOR_H
+#define _JAILHOUSE_PROCESSOR_H
 
 #include <asm/processor.h>
 
 unsigned long phys_processor_id(void);
+
+#endif
