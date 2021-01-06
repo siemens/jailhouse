siemens/jailhouse

commit 67e4de38a3f8c37a429f68019a79a2c387d66fa4

driver: Adjust to kernel 5.8 and 5.9
Several things changed and require tuning:

 - __get_vm_area is gone, switch to __get_vm_area_caller which now
   requires another kernel patch to export it (pointless to try
   RESOLVE_EXTERNAL_SYMBOL since 5.7)

 - cr4_init_shadow was hidden from modules; set/clear VMXE directly
   instead so that the shadow is aligned to the real state; no action
   needed on AMD as EFER is carrying SVME

 - include asm/apic.h explicitly for lapic_timer_period

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

driver/main.c
@@ -42,6 +42,7 @@
 #endif
 #ifdef CONFIG_X86
 #include <asm/msr.h>
+#include <asm/apic.h>
 #endif
 
 #include "cell.h"
@@ -199,6 +200,12 @@ static long get_max_cpus(u32 cpu_set_size,
 	return -EINVAL;
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,8,0)
+#define __get_vm_area(size, flags, start, end)			\
+	__get_vm_area_caller(size, flags, start, end,		\
+			     __builtin_return_address(0))
+#endif
+
 void *jailhouse_ioremap(phys_addr_t phys, unsigned long virt,
 			unsigned long size)
 {
@@ -250,7 +257,13 @@ static void enter_hypervisor(void *info)
 
 #if defined(CONFIG_X86) && LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
 	/* on Intel, VMXE is now on - update the shadow */
-	cr4_init_shadow();
+	if (boot_cpu_has(X86_FEATURE_VMX)) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,5,0)
+		cr4_set_bits_irqsoff(X86_CR4_VMXE);
+#else
+		cr4_set_bits(X86_CR4_VMXE);
+#endif
+	}
 #endif
 
 	atomic_inc(&call_done);
@@ -653,7 +666,13 @@ static void leave_hypervisor(void *info)
 
 #if defined(CONFIG_X86) && LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
 	/* on Intel, VMXE is now off - update the shadow */
-	cr4_init_shadow();
+	if (boot_cpu_has(X86_FEATURE_VMX)) {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,5,0)
+		cr4_clear_bits_irqsoff(X86_CR4_VMXE);
+#else
+		cr4_clear_bits(X86_CR4_VMXE);
+#endif
+	}
 #endif
 
 	atomic_inc(&call_done);

commit beb3ae8a643587023b6a944aab5790f453e12d74

driver: Fix x86 cr4 shadow update on errors
We must not touch the cr4 shadow if initialization or shutdown failed or
it gets out of sync and blocks re-enabling or even reboots.

Fixes: 67e4de38a3f8 ("driver: Adjust to kernel 5.8 and 5.9")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

driver/main.c
@@ -257,7 +257,7 @@ static void enter_hypervisor(void *info)
 
 #if defined(CONFIG_X86) && LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
 	/* on Intel, VMXE is now on - update the shadow */
-	if (boot_cpu_has(X86_FEATURE_VMX)) {
+	if (boot_cpu_has(X86_FEATURE_VMX) && !err) {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,5,0)
 		cr4_set_bits_irqsoff(X86_CR4_VMXE);
 #else
@@ -666,7 +666,7 @@ static void leave_hypervisor(void *info)
 
 #if defined(CONFIG_X86) && LINUX_VERSION_CODE >= KERNEL_VERSION(4,0,0)
 	/* on Intel, VMXE is now off - update the shadow */
-	if (boot_cpu_has(X86_FEATURE_VMX)) {
+	if (boot_cpu_has(X86_FEATURE_VMX) && !err) {
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,5,0)
 		cr4_clear_bits_irqsoff(X86_CR4_VMXE);
 #else