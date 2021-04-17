siemens/jailhouse

commit 7cffb9b7d54dd3466b320f5467bd39110b35ac61

core: fix hugepage splitting in paging_destroy
When unmapping pages, it is not sufficient to compare the size of the
page to the size of the region to be unmapped to know whether a
hugepage needs to be split. That approach does not split hugepages
when the region to be unmapped is larger than a hugepage but does not
cover the entire hugepage, resulting in areas outside the region to be
unmapped and leaving areas inside the region mapped.

Instead of comparing the size of the region to the size of the page,
check if the region overlaps only part of the page and split the
hugepage if it does.

Fixes: 1f7784032531 ("core: Add support for creating page tables with hugepages")
Signed-off-by: Chase Conklin <chase.conklin@arm.com>
[Jan: reduced by two local variables]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/paging.c
@@ -389,7 +389,19 @@ int paging_destroy(const struct paging_structures *pg_structs,
 			if (!paging->entry_valid(pte, PAGE_PRESENT_FLAGS))
 				break;
 			if (paging->get_phys(pte, virt) != INVALID_PHYS_ADDR) {
-				if (paging->page_size <= size)
+				unsigned long page_start;
+
+				/*
+				 * If the region to be unmapped doesn't fully
+				 * cover the hugepage, the hugepage will need to
+				 * be split.
+				 */
+				page_size = paging->page_size ?
+					paging->page_size : PAGE_SIZE;
+				page_start = virt & ~(page_size-1);
+
+				if (virt <= page_start &&
+				    virt + size >= page_start + page_size)
 					break;
 
 				err = split_hugepage(pg_structs->hv_paging,

commit 87aa37d4236dedd81c1c9219ef3f5e6713d42d52

core: avoid overflow in paging_destroy
It is possible for the ending address in paging_destroy to overflow to
exactly 0. For example, in a build with 32-bit addresses, a request to
unmap 512MB at 0xe0000000 will result in an overflow (0xe0000000 +
0x20000000 = 0x100000000 which exceeds 32 bits).

This overflow can be avoided by comparing the last address in the
region to be unmapped rather than the first address to not be
unmapped.

Fixes: 7cffb9b7d54d ("core: fix hugepage splitting in paging_destroy")
Signed-off-by: Chase Conklin <chase.conklin@arm.com>
[Jan: tweak comment to address also size == 0 case]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/paging.c
@@ -399,8 +399,21 @@ int paging_destroy(const struct paging_structures *pg_structs,
 					paging->page_size : PAGE_SIZE;
 				page_start = virt & ~(page_size-1);
 
+				/*
+				 * It's possible that virt + size overflows to
+				 * exactly 0 (e.g. a 512MB region starting at
+				 * 0xe0000000 with 32-bit addresses) during
+				 * normal execution. Any overflow beyond that is
+				 * a programming error.
+				 *
+				 * To handle this case, subtract 1 from the size
+				 * when comparing both sides. Note that size and
+				 * page_size are always > 0, so there's no risk
+				 * of underflow.
+				 */
 				if (virt <= page_start &&
-				    virt + size >= page_start + page_size)
+				    virt + (size - 1) >=
+				    page_start + (page_size - 1))
 					break;
 
 				err = split_hugepage(pg_structs->hv_paging,