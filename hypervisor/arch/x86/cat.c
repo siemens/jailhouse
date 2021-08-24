siemens/jailhouse

commit 3f04eb1753bbbc959f6843e723ac1f7d901aea13

x86: Introduce Cache Allocation Technology support for Intel CPUs
CAT is a CPU feature first added to Xeon D and certain Xeon E5 v3
processors. It so far allows to specify access restrictions to the L3
cache, including complete isolation between different entities.

This adds CAT control to Jailhouse on a per-cell level. The user is free
to specify a contiguous access mask for each cell, use that mask
exclusively (typical case), share any overlaps with the root cell
(JAILHOUSE_CACHE_ROOTSHARED), or simply use the root cell mask. If
nothing else is specified, the root cell uses the full cache (until
non-root cells shrink it).

Due to the hardware-induced requirement to have a contiguous bitmask,
shrinking the root mask on cell creation and extending it again on
destruction is not trivial. Not at all.

When creating a new cell, we may punch a hole into the root mask. In
that case, we also remove the lower half from the roor mask and
accumulate those bits in a "freed mask" for reuse once the hole closes
again. And if we are unlucky, adding a cell empties the current root
mask. Then we have to look into the freed mask and switch to it if it's
non-empty.

When restoring the root mask on cell destruction, we choose a simple
algorithm that first collects all released bits in the freed mask, then
try to merge that mask bit-wise with the current root cell mask. On
success we restart the freed mask walk to ensure that all contiguous
bits are merged.

One may wonder why not reallocating masks completely dynamically and
automatically on each reconfiguration, instead of requiring that
explicit allocation via the config? The reason is that we do not want to
invalidate cache allocations of those cells that are not involved in a
reconfiguration.

A lot of complication with this mechanism which looked so simple on
first sight. Let's just hope that there is a noteworthy benefit in
restricting CAT bitmasks in hardware this way.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/cat.c
@@ -1,0 +1,222 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2015, 2016
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/control.h>
+#include <jailhouse/printk.h>
+#include <jailhouse/utils.h>
+#include <asm/cat.h>
+
+#include <jailhouse/cell-config.h>
+
+#define CAT_ROOT_COS	0
+
+static unsigned int cbm_max, freed_mask;
+static int cos_max = -1;
+static u64 orig_root_mask;
+
+int cat_init(void)
+{
+	int err;
+
+	if (cpuid_ebx(7, 0) & X86_FEATURE_CAT &&
+	    cpuid_ebx(0x10, 0) & (1 << CAT_RESID_L3)) {
+		cbm_max = cpuid_eax(0x10, CAT_RESID_L3) & CAT_CBM_LEN_MASK;
+		cos_max = cpuid_edx(0x10, CAT_RESID_L3) & CAT_COS_MAX_MASK;
+	}
+
+	err = cat_cell_init(&root_cell);
+	orig_root_mask = root_cell.arch.cat_mask;
+
+	return err;
+}
+
+void cat_update(void)
+{
+	struct cell *cell = this_cell();
+
+	write_msr(MSR_IA32_PQR_ASSOC,
+		  (u64)cell->arch.cos << PQR_ASSOC_COS_SHIFT);
+	write_msr(MSR_IA32_L3_MASK_0 + cell->arch.cos, cell->arch.cat_mask);
+}
+
+/* root cell has to be stopped */
+static void cat_update_cell(struct cell *cell)
+{
+	unsigned int cpu;
+
+	for_each_cpu(cpu, cell->cpu_set)
+		if (cpu == this_cpu_id())
+			cat_update();
+		else
+			per_cpu(cpu)->update_cat = true;
+}
+
+static u32 get_free_cos(void)
+{
+	struct cell *cell;
+	u32 cos = 0;
+
+retry:
+	for_each_cell(cell)
+		if (cell->arch.cos == cos) {
+			cos++;
+			goto retry;
+		}
+
+	return cos;
+}
+
+static bool merge_freed_mask_to_root(void)
+{
+	bool updated = false;
+	unsigned int n;
+	u64 bit;
+
+restart:
+	for (n = 0, bit = 1; n < 64; n++, bit <<= 1)
+		/* unless the root mask is empty, merge only neighboring bits */
+		if (freed_mask & bit && (root_cell.arch.cat_mask & (bit << 1) ||
+		     root_cell.arch.cat_mask & (bit >> 1) ||
+		     root_cell.arch.cat_mask == 0)) {
+			root_cell.arch.cat_mask |= bit;
+			freed_mask &= ~bit;
+			updated = true;
+
+			goto restart;
+		}
+
+	return updated;
+}
+
+static bool shrink_root_cell_mask(u64 cell_mask)
+{
+	unsigned int lo_mask_start, lo_mask_len;
+	u64 lo_mask;
+
+	if ((root_cell.arch.cat_mask & ~cell_mask) == 0) {
+		/*
+		 * Try to refill the root mask from the freed mask. The root
+		 * mask must not become empty, so check this first.
+		 */
+		if (freed_mask == 0)
+			return false;
+
+		root_cell.arch.cat_mask = 0;
+		merge_freed_mask_to_root();
+	} else {
+		/* Shrink the root cell's mask. */
+		root_cell.arch.cat_mask &= ~cell_mask;
+
+		/*
+		 * Ensure that the root mask is still contiguous:
+		 *
+		 * Check if taking out the new cell's mask from the root mask
+		 * created two halves there. Then shrink the root mask
+		 * additionally by the lower half and add that part to the
+		 * freed mask.
+		 *
+		 * Always removing the lower half simplifies this algorithm at
+		 * the price of possibly choosing the smaller sub-mask. Cell
+		 * configurations can avoid this by locating non-root cell
+		 * masks at the beginning of the L3 cache.
+		 */
+		lo_mask_start = ffsl(root_cell.arch.cat_mask);
+		lo_mask_len = ffzl(root_cell.arch.cat_mask >> lo_mask_start);
+		lo_mask = BIT_MASK(lo_mask_start + lo_mask_len - 1,
+				   lo_mask_start);
+
+		if (root_cell.arch.cat_mask & ~lo_mask) {
+			root_cell.arch.cat_mask &= ~lo_mask;
+			freed_mask |= lo_mask;
+		}
+	}
+
+	printk("CAT: Shrunk root cell bitmask to %08x\n",
+	       root_cell.arch.cat_mask);
+	cat_update_cell(&root_cell);
+
+	/* Drop this mask from the freed mask in case it was queued there. */
+	freed_mask &= ~cell_mask;
+
+	return true;
+}
+
+int cat_cell_init(struct cell *cell)
+{
+	const struct jailhouse_cache *cache;
+
+	cell->arch.cos = CAT_ROOT_COS;
+
+	if (cos_max < 0)
+		return 0;
+
+	if (cell->config->num_cache_regions > 0) {
+		if (cell != &root_cell) {
+			cell->arch.cos = get_free_cos();
+			if (cell->arch.cos > cos_max)
+				return trace_error(-EBUSY);
+		}
+
+		cache = jailhouse_cell_cache_regions(cell->config);
+
+		if (cell->config->num_cache_regions != 1 ||
+		    cache->type != JAILHOUSE_CACHE_L3 ||
+		    cache->size == 0 || (cache->start + cache->size) > cbm_max)
+			return trace_error(-EINVAL);
+
+		cell->arch.cat_mask =
+			BIT_MASK(cache->start + cache->size - 1, cache->start);
+
+		if (cell != &root_cell &&
+		    !(cache->flags & JAILHOUSE_CACHE_ROOTSHARED) &&
+		    (root_cell.arch.cat_mask & cell->arch.cat_mask) != 0)
+			if (!shrink_root_cell_mask(cell->arch.cat_mask))
+				return trace_error(-EINVAL);
+
+		cat_update_cell(cell);
+	} else {
+		/*
+		 * The root cell always occupies COS0, using the whole cache if
+		 * no restriction is specified. Cells without own cache regions
+		 * share these settings.
+		 */
+		cell->arch.cat_mask = (cell == &root_cell) ?
+			BIT_MASK(cbm_max, 0) : root_cell.arch.cat_mask;
+	}
+
+	printk("CAT: Using COS %d with bitmask %08x for cell %s\n",
+	       cell->arch.cos, cell->arch.cat_mask, cell->config->name);
+
+	return 0;
+}
+
+void cat_cell_exit(struct cell *cell)
+{
+	/*
+	 * Only release the mask of cells with an own partition.
+	 * cos is also CAT_ROOT_COS if CAT is unsupported.
+	 */
+	if (cell->arch.cos == CAT_ROOT_COS)
+		return;
+
+	/*
+	 * Queue bits of released mask for returning to root that were in the
+	 * original root mask as well.
+	 */
+	freed_mask |= cell->arch.cat_mask & orig_root_mask;
+
+	if (merge_freed_mask_to_root()) {
+		printk("CAT: Extended root cell bitmask to %08x\n",
+		       root_cell.arch.cat_mask);
+		cat_update_cell(&root_cell);
+	}
+}

hypervisor/arch/x86/Makefile
@@ -21,5 +21,5 @@ always := $(BUILT_IN_OBJECTS)
 
 obj-y := $(BUILT_IN_OBJECTS)
 
-built-in-amd-y := $(COMMON_OBJECTS) svm.o amd_iommu.o svm-vmexit.o
-built-in-intel-y := $(COMMON_OBJECTS) vmx.o vtd.o vmx-vmexit.o
+built-in-amd-y := $(COMMON_OBJECTS) svm.o amd_iommu.o svm-vmexit.o cat-stubs.o
+built-in-intel-y := $(COMMON_OBJECTS) vmx.o vtd.o vmx-vmexit.o cat.o

hypervisor/arch/x86/cat-stubs.c
@@ -1,0 +1,31 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2015
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <asm/cat.h>
+
+int cat_init(void)
+{
+	return 0;
+}
+
+void cat_update(void)
+{
+}
+
+int cat_cell_init(struct cell *cell)
+{
+	return 0;
+}
+
+void cat_cell_exit(struct cell *cell)
+{
+}

hypervisor/arch/x86/control.c
@@ -15,6 +15,7 @@
 #include <jailhouse/printk.h>
 #include <jailhouse/processor.h>
 #include <asm/apic.h>
+#include <asm/cat.h>
 #include <asm/control.h>
 #include <asm/ioapic.h>
 #include <asm/iommu.h>
@@ -51,6 +52,10 @@ int arch_cell_create(struct cell *cell)
 	if (err)
 		goto error_pci_exit;
 
+	err = cat_cell_init(cell);
+	if (err)
+		goto error_ioapic_exit;
+
 	cell->comm_page.comm_region.pm_timer_address =
 		system_config->platform_info.x86.pm_timer_address;
 	cell->comm_page.comm_region.num_cpus = 0;
@@ -59,6 +64,8 @@ int arch_cell_create(struct cell *cell)
 
 	return 0;
 
+error_ioapic_exit:
+	ioapic_cell_exit(cell);
 error_pci_exit:
 	pci_cell_exit(cell);
 error_iommu_exit:
@@ -113,6 +120,7 @@ void arch_flush_cell_vcpu_caches(struct cell *cell)
 
 void arch_cell_destroy(struct cell *cell)
 {
+	cat_cell_exit(cell);
 	ioapic_cell_exit(cell);
 	pci_cell_exit(cell);
 	iommu_cell_exit(cell);
@@ -262,6 +270,11 @@ void x86_check_events(void)
 	if (cpu_data->flush_vcpu_caches) {
 		cpu_data->flush_vcpu_caches = false;
 		vcpu_tlb_flush();
+	}
+
+	if (cpu_data->update_cat) {
+		cpu_data->update_cat = false;
+		cat_update();
 	}
 
 	spin_unlock(&cpu_data->control_lock);

hypervisor/arch/x86/include/asm/cat.h
@@ -1,0 +1,20 @@
+/*
+ * Jailhouse, a Linux-based partitioning hypervisor
+ *
+ * Copyright (c) Siemens AG, 2015
+ *
+ * Authors:
+ *  Jan Kiszka <jan.kiszka@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ */
+
+#include <jailhouse/cell.h>
+
+int cat_init(void);
+
+void cat_update(void);
+
+int cat_cell_init(struct cell *cell);
+void cat_cell_exit(struct cell *cell);

hypervisor/arch/x86/include/asm/cell.h
@@ -59,6 +59,11 @@ struct arch_cell {
 	struct cell_ioapic *ioapics;
 	/** Number of assigned IOAPICs. */
 	unsigned int num_ioapics;
+
+	/** Class Of Service for cache allocation (Intel only). */
+	u32 cos;
+	/** Allocated L3 cache region (Intel only). */
+	u64 cat_mask;
 };
 
 #endif /* !_JAILHOUSE_ASM_CELL_H */

hypervisor/arch/x86/include/asm/percpu.h
@@ -127,8 +127,10 @@ struct per_cpu {
 	/** Pending SIPI vector; -1 if none is pending. */
 	int sipi_vector;
 	/** Set to true for a pending TLB flush for the paging layer that does
-	 *  host physical <-> guest physical memory mappings */
+	 *  host physical <-> guest physical memory mappings. */
 	bool flush_vcpu_caches;
+	/** Set to true for pending cache allocation updates (Intel only). */
+	bool update_cat;
 	/** Set to true for instructing the CPU to disable hypervisor mode. */
 	bool shutdown_cpu;
 	/** State of the shutdown process. Possible values:

hypervisor/arch/x86/include/asm/processor.h
@@ -27,6 +27,9 @@
 #define X86_FEATURE_XSAVE				(1 << 26)
 #define X86_FEATURE_HYPERVISOR				(1 << 31)
 
+/* leaf 0x07, subleaf 0, EBX */
+#define X86_FEATURE_CAT					(1 << 15)
+
 /* leaf 0x80000001, ECX */
 #define X86_FEATURE_SVM					(1 << 2)
 
@@ -86,6 +89,8 @@
 #define MSR_X2APIC_BASE					0x00000800
 #define MSR_X2APIC_ICR					0x00000830
 #define MSR_X2APIC_END					0x0000083f
+#define MSR_IA32_PQR_ASSOC				0x00000c8f
+#define MSR_IA32_L3_MASK_0				0x00000c90
 #define MSR_EFER					0xc0000080
 #define MSR_STAR					0xc0000081
 #define MSR_LSTAR					0xc0000082
@@ -105,6 +110,13 @@
 #define EFER_LME					0x00000100
 #define EFER_LMA					0x00000400
 #define EFER_NXE					0x00000800
+
+#define PQR_ASSOC_COS_SHIFT				32
+
+#define CAT_RESID_L3					1
+
+#define CAT_CBM_LEN_MASK				BIT_MASK(4, 0)
+#define CAT_COS_MAX_MASK				BIT_MASK(15, 0)
 
 #define GDT_DESC_NULL					0
 #define GDT_DESC_CODE					1

commit aee017cd0c3abfdc94951037435078cf57fbcb53

x86/cat.c: Fix off-by-one error
The variable cbm_max captures the position of the most significant bit
of the CBM, rather than the number of bits in the CBM. This should be
considered when checking non-root cell configurations to be valid,
otherwise the non-root cells can never access the top-most bit in the
CBM.

This off-by-one error ensured that the ROOT_COS CBM always had at least
one private bit in the CBM, preventing it from becoming empty. However,
empty ROOT_COS CBMs are well accounted for in the code.

Fixes: 3f04eb1753bb ("x86: Introduce Cache Allocation Technology support
for Intel CPUs")

Signed-off-by: Bram Hooimeijer <bram.hooimeijer@prodrive-technologies.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/cat.c
@@ -156,7 +156,8 @@ static int cat_cell_init(struct cell *cell)
 
 		if (cell->config->num_cache_regions != 1 ||
 		    cache->type != JAILHOUSE_CACHE_L3 ||
-		    cache->size == 0 || (cache->start + cache->size) > cbm_max)
+		    cache->size == 0 ||
+		    (cache->start + cache->size - 1) > cbm_max)
 			return trace_error(-EINVAL);
 
 		cell->arch.cat_mask =