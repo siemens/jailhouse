siemens/jailhouse

commit 3e9c91302ee5114284227e298883c1098f91001f

x86: Push MSR statistic down to the dispatching points
This is another step in preparing more fine-grained statistics.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/vmx.c
@@ -1189,9 +1189,9 @@ void vcpu_handle_exit(struct per_cpu *cpu_data)
 			return;
 		break;
 	case EXIT_REASON_MSR_WRITE:
-		stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		if (cpu_data->guest_regs.rcx == MSR_IA32_PERF_GLOBAL_CTRL) {
 			/* ignore writes */
+			stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 			vcpu_skip_emulated_instruction(X86_INST_LEN_WRMSR);
 			return;
 		} else if (vcpu_handle_msr_write())

hypervisor/arch/x86/apic.c
@@ -546,8 +546,11 @@ unsigned int apic_mmio_access(const struct guest_paging_structures *pg_structs,
 bool x2apic_handle_write(void)
 {
 	union registers *guest_regs = &this_cpu_data()->guest_regs;
+	u32 *stats = this_cpu_public()->stats;
 	u32 reg = guest_regs->rcx - MSR_X2APIC_BASE;
 	u32 val = guest_regs->rax;
+
+	stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 
 	if (apic_accessing_reserved_bits(reg, val))
 		return false;
@@ -570,6 +573,9 @@ void x2apic_handle_read(void)
 {
 	union registers *guest_regs = &this_cpu_data()->guest_regs;
 	u32 reg = guest_regs->rcx - MSR_X2APIC_BASE;
+	u32 *stats = this_cpu_public()->stats;
+
+	stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 
 	if (reg == APIC_REG_ID)
 		guest_regs->rax = apic_ops.read_id();

hypervisor/arch/x86/svm.c
@@ -795,6 +795,7 @@ static bool svm_handle_msr_write(struct per_cpu *cpu_data)
 	unsigned long efer;
 
 	if (cpu_data->guest_regs.rcx == MSR_EFER) {
+		cpu_data->public.stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		/* Never let a guest to disable SVME; see APMv2, Sect. 3.1.7 */
 		efer = get_wrmsr_value(&cpu_data->guest_regs) | EFER_SVME;
 		/* Flush TLB on LME/NXE change: See APMv2, Sect. 15.16 */
@@ -924,7 +925,6 @@ void vcpu_handle_exit(struct per_cpu *cpu_data)
 		vcpu_handle_cpuid();
 		goto vmentry;
 	case VMEXIT_MSR:
-		cpu_public->stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		if (!vmcb->exitinfo1)
 			res = vcpu_handle_msr_read();
 		else

hypervisor/arch/x86/vcpu.c
@@ -274,9 +274,11 @@ bool vcpu_handle_msr_read(void)
 		x2apic_handle_read();
 		break;
 	case MSR_IA32_PAT:
+		cpu_data->public.stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		set_rdmsr_value(&cpu_data->guest_regs, cpu_data->pat);
 		break;
 	case MSR_IA32_MTRR_DEF_TYPE:
+		cpu_data->public.stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		set_rdmsr_value(&cpu_data->guest_regs,
 				cpu_data->mtrr_def_type);
 		break;
@@ -302,6 +304,7 @@ bool vcpu_handle_msr_write(void)
 			return false;
 		break;
 	case MSR_IA32_PAT:
+		cpu_data->public.stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		val = get_wrmsr_value(&cpu_data->guest_regs);
 		for (bit_pos = 0; bit_pos < 64; bit_pos += 8) {
 			pa = (val >> bit_pos) & 0xff;
@@ -316,6 +319,7 @@ bool vcpu_handle_msr_write(void)
 			vcpu_vendor_set_guest_pat(val);
 		break;
 	case MSR_IA32_MTRR_DEF_TYPE:
+		cpu_data->public.stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		/*
 		 * This only emulates the difference between MTRRs enabled
 		 * and disabled. When disabled, we turn off all caching by

commit 76aa7ef29366c0f3e4746dbe0021e2d026b60ca1

x86: vmx: Avoid double-accounting of MSR read exits
vcpu_handle_msr_read already takes care of accounting.

Fixes: 3e9c91302ee5 ("x86: Push MSR statistic down to the dispatching points")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

hypervisor/arch/x86/vmx.c
@@ -1173,7 +1173,6 @@ void vcpu_handle_exit(struct per_cpu *cpu_data)
 			return;
 		break;
 	case EXIT_REASON_MSR_READ:
-		stats[JAILHOUSE_CPU_STAT_VMEXITS_MSR_OTHER]++;
 		if (vcpu_handle_msr_read())
 			return;
 		break;