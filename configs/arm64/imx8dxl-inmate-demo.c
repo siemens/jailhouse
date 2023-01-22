siemens/jailhouse

commit f2320d2ee4de384480badbeafc7ad5bf8a4526a1

configs: enrich configs with architecture information
This has two advantages:
  - Automatic architecture detection in python tooling
  - Sledgehammer sanity check when enabling jailhouse / creating cells

This commit just adds the field in the cell/sysconfig, adds the fields
to the configs and aligns the config checker that it will not break for
the moment.

Logic is added later, config revision got increased.

Signed-off-by: Ralf Ramsauer <ralf.ramsauer@oth-regensburg.de>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/arm64/imx8dxl-inmate-demo.c
@@ -23,8 +23,10 @@ struct {
 		.revision = JAILHOUSE_CONFIG_REVISION,
 		.name = "gic-demo",
 #ifdef USE_AARCH32
+		.architecture = JAILHOUSE_ARM,
 		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG | JAILHOUSE_CELL_AARCH32,
 #else
+		.architecture = JAILHOUSE_ARM64,
 		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
 #endif
 		.cpu_set_size = sizeof(config.cpus),

commit ea65df766392b069bf4e4a5fa1efc9f05b3504a9

configs: arm64: imx8dxl: Mark 32-bit demo also as 64-bit config
Coupling the config arch to the target arch was counter-productive as it
prevents checking the non-root cell against its 64-bit root cell. Even
if the inmate will run in 32-bit mode, the config continue to target a
64-bit machine.

Fixes: f2320d2ee4de ("configs: enrich configs with architecture information")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

configs/arm64/imx8dxl-inmate-demo.c
@@ -22,11 +22,10 @@ struct {
 		.signature = JAILHOUSE_CELL_DESC_SIGNATURE,
 		.revision = JAILHOUSE_CONFIG_REVISION,
 		.name = "gic-demo",
+		.architecture = JAILHOUSE_ARM64,
 #ifdef USE_AARCH32
-		.architecture = JAILHOUSE_ARM,
 		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG | JAILHOUSE_CELL_AARCH32,
 #else
-		.architecture = JAILHOUSE_ARM64,
 		.flags = JAILHOUSE_CELL_PASSIVE_COMMREG,
 #endif
 		.cpu_set_size = sizeof(config.cpus),